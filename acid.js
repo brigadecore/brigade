712dc8bcaf199b8ac8811d595161eea548a509b4
Matt Butcher <technosophos@gmail.com>
2017-04-19 17:02:44 -0600

    fix: update gitignore and acid.js

--- acid.js
+++ acid.js
@@ -1,12 +1,17 @@
+// ============================================================================
+// NOTE: This is the actual acid.js file for testing the Acid project.
+// Be careful when editing!
+// ============================================================================
+
 // This is a Go project, so we want to set it up for Go.
 gopath = "/go"
 
 // To set GOPATH correctly, we have to override the default
 // path that Acid sets.
-localPath = gopath + "/src/github.com/" + pushRecord.repository.name
+localPath = gopath + "/src/github.com/" + pushRecord.repository.full_name;
 
 // Create a new job
-job1 = new Job("run-unit-tests");
+job1 = new Job("acid-test");
 
 // Since this is Go, we want a go runner.
 job1.image = "technosophos/acid-go:latest";
@@ -17,24 +22,12 @@ job1.env = {
     "GOPATH": gopath
 };
 
-// Get a couple of secrets out the the "mysecret" secrets.
-job1.secrets = {
-  "DB_USER": "mysecret.username",
-  "DB_PASS": "mysecret.password",
-};
-
 // Run three tasks in order.
 job1.tasks = [
   "go get github.com/Masterminds/glide",
   "glide install",
-  "make test-fast"
+  "make test-unit"
 ];
 
-// Testing some flow control. Normally, at that is necessary is to do the
-// run.
-if (job1.run(pushRecord).waitUntilDone()) {
-  console.log("Job succeeded");
-} else {
-  console.log("Job failed");
-}
-;
+// Run and wait for it to finish.
+job1.run(pushRecord).waitUntilDone();
