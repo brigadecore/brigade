2d544556ed084a2aa1caeb5ca02e89a7ff24428e
Matt Butcher <technosophos@gmail.com>
2017-05-12 17:53:32 -0600

    Updated acid.js

--- acid.js
+++ acid.js
@@ -2,32 +2,49 @@
 // NOTE: This is the actual acid.js file for testing the Acid project.
 // Be careful when editing!
 // ============================================================================
+/* global Job WaitGroup pushRecord */
 
 // This is a Go project, so we want to set it up for Go.
-gopath = "/go"
+var gopath = "/go"
 
 // To set GOPATH correctly, we have to override the default
 // path that Acid sets.
-localPath = gopath + "/src/github.com/" + pushRecord.repository.full_name;
+var localPath = gopath + "/src/github.com/" + pushRecord.repository.full_name;
 
 // Create a new job
-job1 = new Job("acid-test");
+var goBuild = new Job("acid-test");
 
 // Since this is Go, we want a go runner.
-job1.image = "technosophos/acid-go:latest";
+goBuild.image = "technosophos/acid-go:latest";
 
 // Set a few environment variables.
-job1.env = {
+goBuild.env = {
     "DEST_PATH": localPath,
     "GOPATH": gopath
 };
 
 // Run three tasks in order.
-job1.tasks = [
+goBuild.tasks = [
   "go get github.com/Masterminds/glide",
   "glide install",
   "make test-unit"
 ];
 
+var jsLint = new Job("acid-js-build");
+
+jsLint.image = "technosophos/acid-node:latest";
+jsLint.tasks = [
+  "npm install eslint",
+  "make test-js"
+];
+
 // Run and wait for it to finish.
-job1.run(pushRecord);
+goBuild.background();
+jsLint.background();
+
+// Wait for both jobs to finish
+var waiter = new WaitGroup()
+
+waiter.add(jsLint)
+waiter.add(goBuild)
+waiter.wait()
