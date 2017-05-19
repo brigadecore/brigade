31687a218fccac449e22e3ead18c93325e4545e8
Matt Butcher <technosophos@gmail.com>
2017-05-18 23:04:07 -0600

    Update images to use SSH url instead of HTTPS URL

--- acidic/acid-go/rootfs/hook.sh
+++ acidic/acid-go/rootfs/hook.sh
@@ -2,9 +2,9 @@
 
 dest=${DEST_PATH:-src}
 
-echo "Clone ${CLONE_URL}#${HEAD_COMMIT_ID} into ${dest}"
+echo "Clone ${SSH_URL}#${HEAD_COMMIT_ID} into ${dest}"
 
-git clone $CLONE_URL $dest
+git clone $SSH_URL $dest
 cd $dest
 git checkout $HEAD_COMMIT_ID
 
