8e1184a6ab8b94e877ad97904eb336efe985026b
Matt Butcher <technosophos@gmail.com>
2017-05-19 14:11:11 -0600

    Work around newline stripping in env vars

--- acidic/acid-ubuntu/rootfs/gitssh.sh
+++ acidic/acid-ubuntu/rootfs/gitssh.sh
@@ -3,7 +3,7 @@ extra=""
 
 if [ "" != "${ACID_REPO_KEY}" ]; then
   KEY="./id_dsa"
-  echo ${ACID_REPO_KEY} > $KEY
+  echo ${ACID_REPO_KEY} | sed 's/\$/\n/g' > $KEY
   chmod 600 $KEY
   extra="-i $KEY -o StrictHostKeyChecking=no"
 fi
