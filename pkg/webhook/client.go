94471e6f00dff34bccf1ffbfd3d5312a685551a4
Matt Butcher <matt.butcher@microsoft.com>
2017-06-09 14:15:33 -0600

    fix(acid): missing client.go file
    My commit yesterday was missing this file.

---
+++ pkg/webhook/client.go
@@ -0,0 +1,44 @@
+package webhook
+
+import (
+	"context"
+	"fmt"
+
+	"golang.org/x/oauth2"
+
+	"github.com/google/go-github/github"
+)
+
+// State names for GitHub status
+var (
+	StatePending = "pending"
+	StateFailure = "failure"
+	StateError   = "error"
+	StateSuccess = "success"
+)
+
+// ghClient gets a new GitHub client object.
+//
+// It authenticates with an OAUTH2 token.
+func ghClient(token string) *github.Client {
+	t := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
+	c := context.Background()
+	tc := oauth2.NewClient(c, t)
+	return github.NewClient(tc)
+}
+
+// setRepoStatus sets the status on a particular commit in a repo.
+func setRepoStatus(push *PushHook, proj *Project, status *github.RepoStatus) error {
+	if proj.GitHubToken == "" {
+		return fmt.Errorf("status update skipped because no GitHubToken exists on %s", proj.Name)
+	}
+	c := context.Background()
+	client := ghClient(proj.GitHubToken)
+	_, _, err := client.Repositories.CreateStatus(
+		c,
+		push.Repository.Owner.Name,
+		push.Repository.Name,
+		push.HeadCommit.SHA,
+		status)
+	return err
+}
