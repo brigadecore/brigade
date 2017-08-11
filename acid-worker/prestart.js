0646ac917f11063fc897b0fb368c56e4f7ef1853
Matt Butcher <matt.butcher@microsoft.com>
2017-08-11 15:52:54 -0600

    feat(worker): wrap acid script in helper

--- acid-worker/prestart.js
+++ acid-worker/prestart.js
@@ -8,7 +8,11 @@ if (!script || script == "") {
   process.exit(0)
 }
 
+
 var buf = Buffer.from(script, "base64")
-fs.writeFile("src/acid.js", buf, () => {
+let wrapper = "const la = require(\"./libacid\");((events, Job, Group, require) => {" +
+  buf.toString("utf8") +
+  "})(la.events, la.Job, la.Group, () => { throw 'require is disabled' })"
+fs.writeFile("src/acid.js", wrapper, () => {
   console.log("prestart: src/acid.js written")
 })
--- acid-worker/src/acid.js
+++
@@ -1,8 +0,0 @@
-// This is the stub acid file.
-const {events} = require("./libacid")
-
-events.on("push", function(e, p){
-  // TODO: Should this throw an exception so that the pod fails immediately?
-  console.log("got event " + e.type + " for " + p.name)
-  console.log("WARNING: No acid.js file was found.")
-})
