b62a550ad5bbf514a2fccb950898f5cdde682df7
Matt Butcher <matt.butcher@microsoft.com>
2017-09-12 17:56:36 -0600

    feat(git-sidecar): improve the SSH handling
    This increases the security of the SSH configuration so that host key
    verification actually works.

--- git-sidecar/rootfs/clone.sh
+++ git-sidecar/rootfs/clone.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 set -e #o pipefail
+
+# From the SSH key, generate the known hosts file
+if [ "" != "${BRIGADE_REPO_KEY}" ]; then
+  mkdir -p $HOME/.ssh
+  KEY="$HOME/.ssh/id_rsa"
+  echo ${BRIGADE_REPO_KEY} | tr '$' '\n' > $KEY
+  chmod 600 $KEY
+  ssh-keygen -y -f $HOME/.ssh/id_rsa > $HOME/.ssh/id_rsa.pub
+  echo "github.com ssh-rsa $(cat $HOME/.ssh/id_rsa.pub)" >> $HOME/.ssh/known_hosts
+  echo "bitbucket.com ssh-rsa $(cat $HOME/.ssh/id_rsa.pub)" >> $HOME/.ssh/known_hosts
+  # Might need to add more.
+fi
+
+# If we want to force SSH:
+# git config --global url."ssh://git@github.com".insteadOf "https://github.com" || true
+
 git clone $VCS_REPO $VCS_LOCAL_PATH
 cd $VCS_LOCAL_PATH
 git checkout $VCS_REVISION
