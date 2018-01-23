705d7adf2311357aa40ab2cf2679a9492d5d70b9
Matt Butcher <matt.butcher@microsoft.com>
2018-01-23 09:18:05 -0700

    feat(brig): build binaries at release
    This automates the process of building binaries for Brig and uploading
    them to a GitHub release.

--- brigade.js
+++ brigade.js
@@ -4,17 +4,19 @@
 // ============================================================================
 const { events, Job, Group} = require("brigadier")
 
+const goImg = "golang:1.9"
+
 function build(e, project) {
   // This is a Go project, so we want to set it up for Go.
   var gopath = "/go"
 
   // To set GOPATH correctly, we have to override the default
   // path that Brigade sets.
+
   var localPath = gopath + "/src/github.com/" + project.repo.name;
 
-
   // Create a new job to run Go tests
-  var goBuild = new Job("brigade-test", "golang:1.9");
+  var goBuild = new Job("brigade-test", goImg);
 
   // Set a few environment variables.
   goBuild.env = {
@@ -42,7 +44,61 @@ function build(e, project) {
   ];
 
   // Run in parallel
-  Group.runAll([jsTest, goBuild])
+  Group.runAll([jsTest, goBuild]).then( () => {
+    const gh = JSON.parse(e.payload)
+    if (e.event == "push" && gh.ref.startsWith("refs/tags/")) {
+      release(e, p)
+    }
+  })
+}
+
+function release(e, p) {
+  if (!p.secrets.ghToken) {
+    throw new Error("Project must have 'secrets.ghToken' set")
+  }
+
+  const tag = e.commit
+  const binName = "brig"
+  const gopath = "/go"
+  const localPath = gopath + "/src/github.com/" + p.repo.name;
+
+  // Cross-compile binaries for a given release and upload them to GitHub.
+  var cx = new Job("cross-compile", goImg)
+  cx.storage.enabled = true
+
+  parts = p.repo.name.split("/", 2)
+
+  cx.env = {
+    GITHUB_USER: parts[0],
+    GITHUB_REPO: parts[1],
+    GITHUB_TOKEN: p.secrets.ghToken,
+    GOPATH: gopath
+  }
+
+  cx.tasks = [
+    "go get github.com/golang/dep/cmd/dep",
+    "go get github.com/aktau/github-release",
+    // Need to move the source into GOPATH so vendor/ works as desired.
+    "mkdir -p " + localPath,
+    "cp -a /src/* " + localPath,
+    "cp -a /src/.git " + localPath,
+    "cd " + localPath,
+    "ls -lh",
+    "dep ensure",
+    "make build-release",
+    `github-release release -t ${tag} -n "${parts[1]} ${tag}"`
+  ]
+
+  // Upload for each target that we support
+  for (const f of ["linux-amd64", "windows-amd64", "darwin-amd64"]) {
+    const name = binName + "-"+f
+    cx.tasks.push(`github-release upload -f ./bin/${name} -n ${name} -t ${tag}`)
+  }
+
+  console.log(cx.tasks)
+  cx.run().then( res => {
+    console.log(`releases at https://github.com/${p.repo.name}/releases/tag/${tag}`);
+  })
 }
 
 events.on("push", build)
@@ -69,3 +125,6 @@ events.on("image_push", (e, p) => {
     console.log(m)
   }
 })
+
+// The "release" event will attempt to create a release for the given tag, and then attach a binary.
+events.on("release", release)
