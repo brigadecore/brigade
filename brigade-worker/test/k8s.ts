162846ddb859dbd3ecf1e9002d03e60723d85613
Justin Hart <jhart@onyxraven.com>
2018-04-13 08:32:36 -0700

    be ok with empty annotations (instead of not set)

--- brigade-worker/test/k8s.ts
+++ brigade-worker/test/k8s.ts
@@ -189,7 +189,7 @@ describe("k8s", function() {
           let jr = new k8s.JobRunner(j, e, p);
           // Currently, annotations are only created if the init container
           // is specified.
-          assert.notProperty(jr.runner.metadata, "annotations");
+          assert.deepEqual(jr.runner.metadata.annotations, {});
         });
       });
       context("when no cloneURL is set", function() {
@@ -201,7 +201,7 @@ describe("k8s", function() {
           let jr = new k8s.JobRunner(j, e, p);
           // Currently, annotations are only created if the init container
           // is specified.
-          assert.notProperty(jr.runner.metadata, "annotations");
+          assert.deepEqual(jr.runner.metadata.annotations, {});
         });
       });
       context("when SSH key is provided", function() {
