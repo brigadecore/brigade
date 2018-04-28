94a73fb33d9dd2dcf32b34997b760f6b864e524e
jnsone11 <jens.neuse@gmx.de>
2018-04-28 13:59:33 +0200

    Add apiCache object to the store to make it available from the api

--- pkg/storage/kube/store.go
+++ pkg/storage/kube/store.go
@@ -2,17 +2,23 @@ package kube
 
 import (
 	"k8s.io/client-go/kubernetes"
-
 	"github.com/Azure/brigade/pkg/storage"
+	"github.com/Azure/brigade/pkg/storage/kube/apicache"
+	"time"
 )
 
 // store represents a storage engine for a brigade.Project.
 type store struct {
 	client    kubernetes.Interface
 	namespace string
+	apiCache  apicache.ApiCache
 }
 
 // New initializes a new storage backend.
 func New(c kubernetes.Interface, namespace string) storage.Store {
-	return &store{c, namespace}
+	return &store{
+		client:    c,
+		namespace: namespace,
+		apiCache: apicache.New(c,namespace,time.Duration(60) * time.Second),
+	}
 }
