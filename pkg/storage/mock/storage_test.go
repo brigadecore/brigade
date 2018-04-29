31a96eb32d8d213c753f6087a2c3b355c57eda61
jnsone11 <jens.neuse@gmx.de>
2018-04-29 15:46:52 +0200

    Add test invocation of the mocked BlockUntilApiCacheSynced func

--- pkg/storage/mock/storage_test.go
+++ pkg/storage/mock/storage_test.go
@@ -55,4 +55,8 @@ func TestStore(t *testing.T) {
 
 	wl, _ := m.GetWorkerLog(StubWorker)
 	assertSame("GetJobLog", StubLogData, wl)
+
+	if !m.BlockUntilApiCacheSynced(nil){
+		t.Fatal("expected to return true")
+	}
 }
