2acbbad7f14b3d8bd02c44305700deed9d8e7b84
jnsone11 <jens.neuse@gmx.de>
2018-04-29 15:27:45 +0200

    Cleanup apicache/cache.go for readability Structure code mode readable + add some comments.

--- pkg/storage/kube/apicache/cache.go
+++ pkg/storage/kube/apicache/cache.go
@@ -3,73 +3,82 @@ package apicache
 import (
 	"k8s.io/api/core/v1"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/apimachinery/pkg/fields"
 	"time"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/apimachinery/pkg/runtime"
-	"github.com/davecgh/go-spew/spew"
+	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/watch"
+	"github.com/Azure/brigade/pkg/merge"
 )
 
 type ApiCache interface {
+	// get cached secrets filtered by label selectors k/v pairs
 	GetSecretsFilteredBy(labelSelectors map[string]string) []v1.Secret
+	// get cached pods filtered by label selectors k/v pairs
 	GetPodsFilteredBy(labelSelectors map[string]string) []v1.Pod
+	// blocks until the api cache is populated
+	// returns true when cache is synced or false when timeout reached
+	BlockUntilApiCacheSynced(waitUntil <-chan time.Time) bool
 }
 
 type apiCache struct {
-	client      kubernetes.Interface
-	secretStore cache.Store
-	podStore    cache.Store
-}
-
-func New(kubernetesClient kubernetes.Interface, namespace string, resyncPeriod time.Duration) ApiCache {
-	return &apiCache{
-		client: kubernetesClient,
-		secretStore: newListStore(kubernetesClient, storeConfig{
-			resource:     "secrets",
-			namespace:    namespace,
-			resyncPeriod: resyncPeriod,
-			expectedType: &v1.Secret{},
-		}),
-		podStore: newListStore(kubernetesClient, storeConfig{
-			resource:     "pods",
-			namespace:    namespace,
-			resyncPeriod: resyncPeriod,
-			expectedType: &v1.Pod{},
-		}),
-	}
+	// the kubernetes client
+	client             kubernetes.Interface
+	// a ready to use cache.Store for secrets
+	secretStore        cache.Store
+	// a ready to use cache.Store for pods
+	podStore           cache.Store
+	// a chan which is going to be closed after the ApiCache has initially synced all cache.Store's
+	hasSyncedInitially <-chan struct{}
 }
 
 type storeConfig struct {
+	// the kubernetes resource to listen to (e.g. 'pods', 'secrets', etc.)
 	resource     string
+	// the kubernetes namespace to filter by
 	namespace    string
+	// how often to re-sync
 	resyncPeriod time.Duration
+	// which type to expect when new synced objects arrive
 	expectedType runtime.Object
+	// implement the method invokind the kubernetes.Interface to return
+	// a List of the expected runtime.Object type
+	listFunc     func(client kubernetes.Interface, namespace string, options metaV1.ListOptions) (runtime.Object, error)
+	// implement the method invoking the kubernetes.Interface to return
+	// a watch.Interface that returns the expected runtime.Object type
+	watchFunc    func(client kubernetes.Interface, namespace string, options metaV1.ListOptions) (watch.Interface, error)
 }
 
-func newListStore(client kubernetes.Interface, config storeConfig) cache.Store {
+func New(client kubernetes.Interface, namespace string, resyncPeriod time.Duration) ApiCache {
 
-	listWatch := cache.NewListWatchFromClient(
-		client.CoreV1().RESTClient(),
-		config.resource,
-		config.namespace,
-		fields.Everything())
+	secretsSynced := make(chan struct{})
+	podsSynced := make(chan struct{})
 
-	store, ctr := cache.NewInformer(
-		listWatch,
-		config.expectedType,
-		config.resyncPeriod,
-		cache.ResourceEventHandlerFuncs{
-			AddFunc: func(obj interface{}) {
-				spew.Dump("object added",obj)
-			},
-			UpdateFunc: func(oldObj, newObj interface{}) {
-				spew.Dump("object updated",oldObj,newObj)
-			},
-			DeleteFunc: func(obj interface{}) {
-				spew.Dump("object deleted",obj)
-			},
-		})
+	merged := merge.Channels(secretsSynced, podsSynced)
 
-	go ctr.Run(nil)
-	return store
+	return &apiCache{
+		hasSyncedInitially: merged,
+		client:             client,
+		secretStore:        secretStoreFactory{}.new(client, namespace, resyncPeriod, secretsSynced),
+		podStore:           podStoreFactory{}.new(client, namespace, resyncPeriod, podsSynced),
+	}
+}
+
+// Blocks until all cache.Store's are synced
+// returns true if all channels are closed
+// returns false if the timeout was reached (in case waitUntil is not nil)
+func (a *apiCache) BlockUntilApiCacheSynced(waitUntil <-chan time.Time) bool {
+	if waitUntil == nil {
+		<-a.hasSyncedInitially
+		return true
+	} else {
+		select {
+		case <-waitUntil:
+			return false
+		case _, ok := <-a.hasSyncedInitially:
+			// ok == false when all merged channels are closed
+			// therefore return !ok to indicate every listener is synced if that's the case
+			return !ok
+		}
+	}
 }
