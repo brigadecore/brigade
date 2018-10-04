ed39ffbf8e22d5a4f3a3c9e1bb535c29f6a254eb
Ben Limmer <ben@ibotta.com>
2018-10-04 12:42:39 -0600

    Fix invalid_format for Git SSH Keys. (#647)
    Fixes #646

--- git-sidecar/rootfs/gitssh.sh
+++ git-sidecar/rootfs/gitssh.sh
@@ -3,7 +3,7 @@ extra=""
 
 if [ "" != "${BRIGADE_REPO_KEY}" ]; then
   KEY="./id_dsa"
-  echo ${BRIGADE_REPO_KEY} | sed 's/\$/\n/g' > $KEY
+  printf "%s" "$BRIGADE_REPO_KEY" | sed 's/\$/\n/g' > $KEY
   chmod 600 $KEY
   extra="-i $KEY -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
 fi
