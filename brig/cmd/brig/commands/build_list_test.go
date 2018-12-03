fbfb52d5a02ee51d1ec16c9ade0c53c76785803d
Dimitris-Ilias Gkanatsios <dgkanatsios@outlook.com>
2018-12-03 11:22:58 +0200

    refactored test methods

--- brig/cmd/brig/commands/build_list_test.go
+++ brig/cmd/brig/commands/build_list_test.go
@@ -11,7 +11,7 @@ import (
 )
 
 const (
-	stubProjectID  = "brigade-544b459e6ad7267e7791c4f77bfd1722a15e305a22cf9d3c60c5be"
+	stubProject1ID = "brigade-544b459e6ad7267e7791c4f77bfd1722a15e305a22cf9d3c60c5be"
 	stubProject2ID = "brigade-544b459e6ad7267e7791c4f77bfd1722a15e305a22cf9d3c60c5bf"
 	stubBuild1ID   = "01cxmy71nbq7nasvth8pva1s21"
 	stubBuild2ID   = "01cxmy71nbq7nasvth8pva1s22"
@@ -59,13 +59,13 @@ func TestGetBuildList(t *testing.T) {
 func TestGetBuildListWithProject(t *testing.T) {
 	client := fake.NewSimpleClientset()
 	createFakeBuilds(t, client)
-	bls, err := getBuilds(stubProjectID, client, 0)
+	bls, err := getBuilds(stubProject1ID, client, 0)
 	if err != nil {
 		t.Error(err)
 	}
 
 	if len(bls) != 2 {
-		t.Errorf("Error in getBuilds for project %s", stubProjectID)
+		t.Errorf("Error in getBuilds for project %s", stubProject1ID)
 	}
 
 	if bls[0].since != "???" || bls[0].ID != stubBuild2ID {
@@ -103,180 +103,27 @@ func TestGetBuildListCountTwo(t *testing.T) {
 // Build2 started 1 minute ago and still running
 // Build3 started 3 minutes ago and finished 1 minute ago and belongs to a different project than build1 and build2
 func createFakeBuilds(t *testing.T, client kubernetes.Interface) {
-
-	stubProjectSecret := &v1.Secret{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: stubProjectID,
-			Labels: map[string]string{
-				"project":   stubProjectID,
-				"component": "project",
-				"heritage":  "brigade",
-				"app":       "brigade",
-			},
-			Annotations: map[string]string{
-				"projectName": "deis/empty-testbed",
-			},
-		},
-		Type: "brigade.sh/build",
-	}
-
-	stubWorker1Pod := &v1.Pod{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "some-id-" + stubBuild1ID,
-			Labels: map[string]string{
-				"build":     stubBuild1ID,
-				"project":   stubProjectID,
-				"component": "build",
-				"heritage":  "brigade",
-			},
-			CreationTimestamp: metav1.NewTime(stubDT1Start),
-		},
-		Spec: v1.PodSpec{
-			Containers: []v1.Container{
-				{
-					Image: "alpine:3.7",
-				},
-			},
-		},
-		Status: v1.PodStatus{
-			Phase:     v1.PodSucceeded,
-			StartTime: &stubTimeDT1Start,
-			ContainerStatuses: []v1.ContainerStatus{
-				{
-					State: v1.ContainerState{
-						Terminated: &v1.ContainerStateTerminated{
-							ExitCode:   0,
-							FinishedAt: metav1.NewTime(stubDT1End),
-						},
-					},
-				},
-			},
-		},
-	}
-
-	stubBuild1Secret := &v1.Secret{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: stubBuild1ID,
-			Labels: map[string]string{
-				"build":     stubBuild1ID,
-				"project":   stubProjectID,
-				"component": "build",
-				"heritage":  "brigade",
-				"app":       "brigade",
-			},
-			Annotations: map[string]string{
-				"projectName": "deis/empty-testbed",
-			},
-		},
-		Type: "brigade.sh/build",
-	}
-
-	stubWorker2Pod := &v1.Pod{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "some-id-" + stubBuild2ID,
-			Labels: map[string]string{
-				"build":     stubBuild2ID,
-				"project":   stubProjectID,
-				"component": "build",
-				"heritage":  "brigade",
-			},
-			CreationTimestamp: metav1.NewTime(stubDT2Start),
-		},
-		Spec: v1.PodSpec{
-			Containers: []v1.Container{
-				{
-					Image: "alpine:3.7",
-				},
-			},
-		},
-		Status: v1.PodStatus{
-			Phase:     v1.PodRunning,
-			StartTime: &stubTimeDT2Start,
-			ContainerStatuses: []v1.ContainerStatus{
-				{
-					State: v1.ContainerState{
-						Running: &v1.ContainerStateRunning{
-							StartedAt: stubTimeDT2Start,
-						},
-					},
-				},
-			},
-		},
-	}
-
-	stubBuild2Secret := &v1.Secret{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: stubBuild2ID,
-			Labels: map[string]string{
-				"build":     stubBuild2ID,
-				"project":   stubProjectID,
-				"component": "build",
-				"heritage":  "brigade",
-				"app":       "brigade",
-			},
-			Annotations: map[string]string{
-				"projectName": "deis/empty-testbed",
-			},
-		},
-		Type: "brigade.sh/build",
-	}
-
-	stubWorker3Pod := &v1.Pod{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "some-id-" + stubBuild3ID,
-			Labels: map[string]string{
-				"build":     stubBuild3ID,
-				"project":   stubProject2ID,
-				"component": "build",
-				"heritage":  "brigade",
-			},
-			CreationTimestamp: metav1.NewTime(stubDT3Start),
-		},
-		Spec: v1.PodSpec{
-			Containers: []v1.Container{
-				{
-					Image: "alpine:3.7",
-				},
-			},
-		},
-		Status: v1.PodStatus{
-			Phase:     v1.PodSucceeded,
-			StartTime: &stubTimeDT3Start,
-			ContainerStatuses: []v1.ContainerStatus{
-				{
-					State: v1.ContainerState{
-						Terminated: &v1.ContainerStateTerminated{
-							ExitCode:   0,
-							FinishedAt: metav1.NewTime(stubDT3End),
-						},
-					},
-				},
-			},
-		},
-	}
-
-	stubBuild3Secret := &v1.Secret{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: stubBuild3ID,
-			Labels: map[string]string{
-				"build":     stubBuild3ID,
-				"project":   stubProject2ID,
-				"component": "build",
-				"heritage":  "brigade",
-				"app":       "brigade",
-			},
-			Annotations: map[string]string{
-				"projectName": "deis/empty-testbed",
-			},
-		},
-		Type: "brigade.sh/build",
-	}
-
-	_, err := client.CoreV1().Secrets("default").Create(stubProjectSecret)
+	stubProject1Secret := createStubProjectSecret(stubProject1ID)
+	_, err := client.CoreV1().Secrets("default").Create(stubProject1Secret)
 	if err != nil {
 		t.Error(err)
 	}
 
+	stubBuild1Secret := createStubBuildSecret(stubProject1ID, stubBuild1ID)
+	stubWorker1Pod := createStubPod(stubProject1ID, stubBuild1ID, stubDT1Start, v1.PodStatus{
+		Phase:     v1.PodSucceeded,
+		StartTime: &stubTimeDT1Start,
+		ContainerStatuses: []v1.ContainerStatus{
+			{
+				State: v1.ContainerState{
+					Terminated: &v1.ContainerStateTerminated{
+						ExitCode:   0,
+						FinishedAt: metav1.NewTime(stubDT1End),
+					},
+				},
+			},
+		},
+	})
 	_, err = client.CoreV1().Pods("default").Create(stubWorker1Pod)
 	if err != nil {
 		t.Error(err)
@@ -286,6 +133,20 @@ func createFakeBuilds(t *testing.T, client kubernetes.Interface) {
 		t.Error(err)
 	}
 
+	stubBuild2Secret := createStubBuildSecret(stubProject1ID, stubBuild2ID)
+	stubWorker2Pod := createStubPod(stubProject1ID, stubBuild2ID, stubDT2Start, v1.PodStatus{
+		Phase:     v1.PodRunning,
+		StartTime: &stubTimeDT2Start,
+		ContainerStatuses: []v1.ContainerStatus{
+			{
+				State: v1.ContainerState{
+					Running: &v1.ContainerStateRunning{
+						StartedAt: stubTimeDT2Start,
+					},
+				},
+			},
+		},
+	})
 	_, err = client.CoreV1().Pods("default").Create(stubWorker2Pod)
 	if err != nil {
 		t.Error(err)
@@ -295,6 +156,21 @@ func createFakeBuilds(t *testing.T, client kubernetes.Interface) {
 		t.Error(err)
 	}
 
+	stubBuild3Secret := createStubBuildSecret(stubProject2ID, stubBuild3ID)
+	stubWorker3Pod := createStubPod(stubProject2ID, stubBuild3ID, stubDT3Start, v1.PodStatus{
+		Phase:     v1.PodSucceeded,
+		StartTime: &stubTimeDT3Start,
+		ContainerStatuses: []v1.ContainerStatus{
+			{
+				State: v1.ContainerState{
+					Terminated: &v1.ContainerStateTerminated{
+						ExitCode:   0,
+						FinishedAt: metav1.NewTime(stubDT3End),
+					},
+				},
+			},
+		},
+	})
 	_, err = client.CoreV1().Pods("default").Create(stubWorker3Pod)
 	if err != nil {
 		t.Error(err)
@@ -302,5 +178,71 @@ func createFakeBuilds(t *testing.T, client kubernetes.Interface) {
 	_, err = client.CoreV1().Secrets("default").Create(stubBuild3Secret)
 	if err != nil {
 		t.Error(err)
+	}
+}
+
+func createStubProjectSecret(projectID string) *v1.Secret {
+	return &v1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: projectID,
+			Labels: map[string]string{
+				"project":   projectID,
+				"component": "project",
+				"heritage":  "brigade",
+				"app":       "brigade",
+			},
+			Annotations: map[string]string{
+				"projectName": "deis/empty-testbed",
+			},
+		},
+		Type: "brigade.sh/project",
+	}
+}
+
+func createStubBuildSecret(projectID string, buildID string) *v1.Secret {
+	secret := v1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: buildID,
+			Labels: map[string]string{
+				"project":   projectID,
+				"build":     buildID,
+				"component": "build",
+				"heritage":  "brigade",
+				"app":       "brigade",
+			},
+			Annotations: map[string]string{
+				"projectName": "deis/empty-testbed",
+			},
+		},
+		Type: "brigade.sh/build",
+	}
+
+	if buildID != "" {
+		secret.ObjectMeta.Labels["build"] = buildID
+		secret.ObjectMeta.Labels["app"] = "brigade"
+	}
+	return &secret
+}
+
+func createStubPod(projectID string, buildID string, startTime time.Time, podStatus v1.PodStatus) *v1.Pod {
+	return &v1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "some-id-" + buildID,
+			Labels: map[string]string{
+				"build":     buildID,
+				"project":   projectID,
+				"component": "build",
+				"heritage":  "brigade",
+			},
+			CreationTimestamp: metav1.NewTime(startTime),
+		},
+		Spec: v1.PodSpec{
+			Containers: []v1.Container{
+				{
+					Image: "alpine:3.7",
+				},
+			},
+		},
+		Status: podStatus,
 	}
 }
