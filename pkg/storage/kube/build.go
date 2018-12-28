ee7d1a60e350450ae9b07bac98097ae678e7f66e
Dimitris-Ilias Gkanatsios <dgkanatsios@outlook.com>
2018-12-28 20:42:28 +0200

    fix linter errors

--- pkg/storage/kube/build.go
+++ pkg/storage/kube/build.go
@@ -12,7 +12,7 @@ import (
 
 	"github.com/oklog/ulid"
 
-	"k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	meta "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
--- pkg/storage/kube/worker_test.go
+++ pkg/storage/kube/worker_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 	"time"
 
-	"k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/Azure/brigade/pkg/brigade"
