bc27385fdf39d03d1184fa7991980ef51dbc0990
Radu Matei <radu@radu-matei.com>
2019-01-29 08:12:33 +0200

    Enable pretty import paths for local imports (from repo) in brigade.js

--- brigade-worker/src/require.ts
+++ brigade-worker/src/require.ts
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const pkgOverrides: Pkg[] = [
   { name: "brigade", override: "./brigadier" },
   { name: "brigadier", override: "./brigadier" },
@@ -19,6 +21,12 @@ export function overridingRequire(pkg: string): any {
   for (let p of pkgOverrides) {
     if (p.name == pkg) {
       return require(p.override);
+    }
+
+    // we want to intercept the loading of relative modules in the repo
+    // and and override with the absolute path inside the worker pod
+    if ((!path.isAbsolute(pkg)) && (pkg.includes("./"))) {
+      return require(pkg.substr(0, pkg.indexOf("./")) + "/vcs" + pkg.substr(pkg.indexOf("./") + 1))
     }
   }
   return require(pkg);
