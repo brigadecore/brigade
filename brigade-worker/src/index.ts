e553b228ab012708c0ba6efa97e7ba63a5deae80
Chris Chambers <chris-chambers@users.noreply.github.com>
2019-10-10 21:59:48 +0200

    Restore pre-1.1.0 module resolution behavior for local dependencies (#986)
    With the introduction of `module-alias`, the ability to require "local"
    dependencies from brigade entry scripts outside of `/vcs` (eg, when running via
    `brig run --file`) was broken.
    
    Now, dot-slash-prefixed requires from the entry script are rewritten like:
    
        './brigade-deps/foo' => '/vcs/brigade-deps/foo'
    
    So that the entry script can properly require them, no matter where the entry
    script is on disk.
    
    Fixes #985
    
    Signed-off-by: Chris Chambers <chris.chambers@peanutcode.com>

--- brigade-worker/src/index.ts
+++ brigade-worker/src/index.ts
@@ -34,6 +34,7 @@
 
 import * as fs from "fs";
 import * as moduleAlias from "module-alias";
+import * as path from "path";
 import * as process from "process";
 import * as ulid from "ulid";
 
@@ -42,18 +43,6 @@ import { App } from "./app";
 import { ContextLogger, LogLevel } from "@brigadecore/brigadier/out/logger";
 
 import { options } from "./k8s";
-
-// Install aliases for common ways of referring to Brigade/Brigadier.
-moduleAlias.addAliases({
-  "brigade": __dirname + "/brigadier",
-  "brigadier": __dirname + "/brigadier",
-  "@brigadecore/brigadier": __dirname + "/brigadier",
-});
-// Add the current module resolution paths to module-alias, so the node_modules
-// that prestart.js adds to will be resolvable from the Brigade script and any
-// local dependencies.
-module.paths.forEach(moduleAlias.addPath);
-moduleAlias();
 
 // Script locations in order of precedence.
 const scripts = [
@@ -84,6 +73,41 @@ function findScript() {
 // Search for the Brigade script and, if found, execute it.
 const script = findScript();
 if (script) {
+  // Install aliases for common ways of referring to Brigade/Brigadier.
+  moduleAlias.addAliases({
+    "brigade": __dirname + "/brigadier",
+    "brigadier": __dirname + "/brigadier",
+    "@brigadecore/brigadier": __dirname + "/brigadier",
+  });
+
+  // Add the current module resolution paths to module-alias, so the node_modules
+  // that prestart.js adds to will be resolvable from the Brigade script and any
+  // local dependencies.
+  module.paths.forEach(moduleAlias.addPath);
+
+  const realScriptPath = fs.realpathSync(script);
+  // NOTE: `as any` is needed because @types/module-alias is at 2.0.0, while
+  //       module-alias is now at 2.2.0.
+  (moduleAlias as any).addAlias(".", (fromPath: string) => {
+    // A custom handler for local dependencies to handle cases where the entry
+    // script is outside `/vcs`.
+
+    // For entry scripts outside /vcs only, rewrite dot-slash-prefixed requires
+    // to be rooted at `/vcs`.
+    if (!fromPath.startsWith("/vcs") && fromPath === realScriptPath) {
+      return "/vcs";
+    }
+
+    // For all other dot-slash-prefixed requires, resolve as usual.
+    // NOTE: module-alias will not allow us to just return "." here, because
+    // it uses path.join under the hood, which collapses "./foo" down to just
+    // "foo", for which the module resolution semantics are different.  So,
+    // return the directory of the requiring module, which gives the same result
+    // as ".".
+    return path.dirname(fromPath);
+  });
+
+  moduleAlias();
   require(script);
 }
 
