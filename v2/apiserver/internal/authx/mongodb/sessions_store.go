f1cb8225ea4dd9dc303b2255987758dc25e5241b
Kent Rancourt <kent.rancourt@microsoft.com>
2020-10-08 10:08:09 -0400

    add api server with session management endpoints
    Signed-off-by: Kent Rancourt <kent.rancourt@microsoft.com>

---
+++ v2/apiserver/internal/authx/mongodb/sessions_store.go
@@ -0,0 +1,121 @@
+package mongodb
+
+import (
+	"context"
+	"time"
+
+	"github.com/brigadecore/brigade/v2/apiserver/internal/authx"
+	"github.com/brigadecore/brigade/v2/apiserver/internal/lib/mongodb"
+	"github.com/brigadecore/brigade/v2/apiserver/internal/meta"
+	"github.com/pkg/errors"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+// sessionsStore is a MongoDB-based implementation of the authx.SessionsStore
+// interface.
+type sessionsStore struct {
+	collection mongodb.Collection
+}
+
+// NewSessionsStore returns a MongoDB-based implementation of the
+// authx.SessionsStore interface.
+func NewSessionsStore(database *mongo.Database) authx.SessionsStore {
+	return &sessionsStore{
+		collection: database.Collection("sessions"),
+	}
+}
+
+func (s *sessionsStore) Create(
+	ctx context.Context,
+	session authx.Session,
+) error {
+	if _, err := s.collection.InsertOne(ctx, session); err != nil {
+		return errors.Wrapf(err, "error inserting new session %q", session.ID)
+	}
+	return nil
+}
+
+func (s *sessionsStore) GetByHashedOAuth2State(
+	ctx context.Context,
+	hashedOAuth2State string,
+) (authx.Session, error) {
+	session := authx.Session{}
+	res := s.collection.FindOne(
+		ctx,
+		bson.M{"hashedOAuth2State": hashedOAuth2State},
+	)
+	err := res.Decode(&session)
+	if err == mongo.ErrNoDocuments {
+		return session, &meta.ErrNotFound{
+			Type: "Session",
+		}
+	}
+	if err != nil {
+		return session, errors.Wrap(err, "error finding/decoding session")
+	}
+	return session, nil
+}
+
+func (s *sessionsStore) GetByHashedToken(
+	ctx context.Context,
+	hashedToken string,
+) (authx.Session, error) {
+	session := authx.Session{}
+	res := s.collection.FindOne(ctx, bson.M{"hashedToken": hashedToken})
+	err := res.Decode(&session)
+	if err == mongo.ErrNoDocuments {
+		return session, &meta.ErrNotFound{
+			Type: "Session",
+		}
+	}
+	if err != nil {
+		return session, errors.Wrap(err, "error finding/decoding session")
+	}
+	return session, nil
+}
+
+func (s *sessionsStore) Authenticate(
+	ctx context.Context,
+	sessionID string,
+	userID string,
+	expires time.Time,
+) error {
+	res, err := s.collection.UpdateOne(
+		ctx,
+		bson.M{
+			"id": sessionID,
+		},
+		bson.M{
+			"$set": bson.M{
+				"userID":        userID,
+				"authenticated": time.Now(),
+				"expires":       expires,
+			},
+		},
+	)
+	if err != nil {
+		return errors.Wrapf(err, "error updating session %q", sessionID)
+	}
+	if res.MatchedCount == 0 {
+		return &meta.ErrNotFound{
+			Type: "Session",
+			ID:   sessionID,
+		}
+	}
+	return nil
+}
+
+func (s *sessionsStore) Delete(ctx context.Context, id string) error {
+	res, err := s.collection.DeleteOne(ctx, bson.M{"id": id})
+	if err != nil {
+		return errors.Wrapf(err, "error deleting session %q", id)
+	}
+	if res.DeletedCount == 0 {
+		return &meta.ErrNotFound{
+			Type: "Session",
+			ID:   id,
+		}
+	}
+	return nil
+}
---
+++ v2/apiserver/internal/authx/mongodb/users_store.go
@@ -0,0 +1,64 @@
+package mongodb
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/brigadecore/brigade/v2/apiserver/internal/authx"
+	"github.com/brigadecore/brigade/v2/apiserver/internal/lib/mongodb"
+	"github.com/brigadecore/brigade/v2/apiserver/internal/meta"
+	"github.com/pkg/errors"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+// usersStore is a a MongoDB-based implementation of the authx.UsersStore
+// interface.
+type usersStore struct {
+	collection mongodb.Collection
+}
+
+// NewUsersStore returns a MongoDB-based implementation of the authx.UsersStore
+// interface.
+func NewUsersStore(database *mongo.Database) authx.UsersStore {
+	return &usersStore{
+		collection: database.Collection("users"),
+	}
+}
+
+func (u *usersStore) Create(ctx context.Context, user authx.User) error {
+	if _, err :=
+		u.collection.InsertOne(ctx, user); err != nil {
+		if writeException, ok := err.(mongo.WriteException); ok {
+			if len(writeException.WriteErrors) == 1 &&
+				writeException.WriteErrors[0].Code == 11000 {
+				return &meta.ErrConflict{
+					Type:   "User",
+					ID:     user.ID,
+					Reason: fmt.Sprintf("A user with the ID %q already exists.", user.ID),
+				}
+			}
+		}
+		return errors.Wrapf(err, "error inserting new user %q", user.ID)
+	}
+	return nil
+}
+
+func (u *usersStore) Get(
+	ctx context.Context,
+	id string,
+) (authx.User, error) {
+	user := authx.User{}
+	res := u.collection.FindOne(ctx, bson.M{"id": id})
+	err := res.Decode(&user)
+	if err == mongo.ErrNoDocuments {
+		return user, &meta.ErrNotFound{
+			Type: "User",
+			ID:   id,
+		}
+	}
+	if err != nil {
+		return user, errors.Wrapf(err, "error finding/decoding user %q", id)
+	}
+	return user, nil
+}
