a56cac88e21a5bcdecdf5103b38f39154b89ae28
Kent Rancourt <kent.rancourt@microsoft.com>
2020-10-29 10:43:03 -0400

    add abstraction for reading messages from a messaging system
    Signed-off-by: Kent Rancourt <kent.rancourt@microsoft.com>

---
+++ v2/scheduler/internal/lib/queue/amqp/reader.go
@@ -0,0 +1,204 @@
+package amqp
+
+import (
+	"context"
+	"log"
+	"sync"
+	"time"
+
+	"github.com/Azure/go-amqp"
+	myamqp "github.com/brigadecore/brigade/v2/internal/amqp"
+	"github.com/brigadecore/brigade/v2/internal/os"
+	"github.com/brigadecore/brigade/v2/internal/retries"
+	"github.com/brigadecore/brigade/v2/scheduler/internal/lib/queue"
+	"github.com/pkg/errors"
+)
+
+// ReaderFactoryConfig encapsulates details required for connecting an
+// AMQP-based implementation of the queue.ReaderFactory interface to an
+// underlying AMQP-based messaging service.
+type ReaderFactoryConfig struct {
+	// Address is the address of the AMQP-based messaging server.
+	Address string
+	// Username is the SASL username to use when connecting to the AMQP-based
+	// messaging server.
+	Username string
+	// Password is the SASL password to use when connection to the AMQP-based
+	// messaging server.
+	Password string
+}
+
+// GetReaderFactoryConfig returns ReaderFactoryConfig based on configuration
+// obtained from environment variables.
+func GetReaderFactoryConfig() (ReaderFactoryConfig, error) {
+	config := ReaderFactoryConfig{}
+	var err error
+	config.Address, err = os.GetRequiredEnvVar("AMQP_ADDRESS")
+	if err != nil {
+		return config, err
+	}
+	config.Username, err = os.GetRequiredEnvVar("AMQP_USERNAME")
+	if err != nil {
+		return config, err
+	}
+	config.Password, err = os.GetRequiredEnvVar("AMQP_PASSWORD")
+	return config, err
+}
+
+// readerFactory is an AMQP-based implementation of the queue.ReaderFactory
+// interface.
+type readerFactory struct {
+	address      string
+	dialOpts     []amqp.ConnOption
+	amqpClient   myamqp.Client
+	amqpClientMu *sync.Mutex
+	connectFn    func() error
+}
+
+// NewReaderFactory returns an an AMQP-based implementation of the
+// queue.ReaderFactory interface.
+func NewReaderFactory(config ReaderFactoryConfig) queue.ReaderFactory {
+	q := &readerFactory{
+		address: config.Address,
+		dialOpts: []amqp.ConnOption{
+			amqp.ConnSASLPlain(config.Username, config.Password),
+		},
+		amqpClientMu: &sync.Mutex{},
+	}
+	q.connectFn = q.connect
+	return q
+}
+
+// connect connects (or reconnects) to the underlying AMQP-based messaging
+// server. This function is NOT concurrency safe and callers should take
+// measures to ensure they are the exclusive caller of this function.
+func (q *readerFactory) connect() error {
+	return retries.ManageRetries(
+		context.Background(),
+		"connect",
+		10,
+		10*time.Second,
+		func() (bool, error) {
+			if q.amqpClient != nil {
+				q.amqpClient.Close()
+			}
+			var err error
+			if q.amqpClient, err = myamqp.Dial(q.address, q.dialOpts...); err != nil {
+				return true, errors.Wrap(err, "error dialing endpoint")
+			}
+			return false, nil
+		},
+	)
+}
+
+func (q *readerFactory) NewReader(queueName string) (queue.Reader, error) {
+	// This entire function is a critical section of code so that we don't
+	// possibly have multiple callers looking for a new Reader opening multiple
+	// underlying connections to the messaging server.
+	q.amqpClientMu.Lock()
+	defer q.amqpClientMu.Unlock()
+
+	if q.amqpClient == nil {
+		if err := q.connectFn(); err != nil {
+			return nil, err
+		}
+	}
+
+	linkOpts := []amqp.LinkOption{
+		amqp.LinkSourceAddress(queueName),
+		// Link credit is 1 because we're a "slow" consumer. We do not want messages
+		// piling up in a client-side buffer, knowing that it could be some time
+		// before we can process them.
+		amqp.LinkCredit(1),
+	}
+
+	// Every Writer will get its own Session and Receiver
+	var amqpSession myamqp.Session
+	var amqpReceiver myamqp.Receiver
+	var err error
+	for {
+		// If we've been through the loop before, try cleaning up the session and/or
+		// receiver that we never ended up using.
+		if amqpReceiver != nil {
+			amqpReceiver.Close(context.TODO()) // nolint: errcheck
+		}
+		if amqpSession != nil {
+			amqpSession.Close(context.TODO()) // nolint: errcheck
+		}
+
+		if amqpSession, err = q.amqpClient.NewSession(); err != nil {
+			// Assume this happened because the existing connection is no good. Try
+			// to reconnect.
+			if err = q.connectFn(); err != nil {
+				// The connection function handles its own retries. If we got an error
+				// here, it's pretty serious. Bail.
+				return nil, err
+			}
+			// We're reconnected now, so loop around to try getting a session again.
+			continue
+		}
+		if amqpReceiver, err = amqpSession.NewReceiver(linkOpts...); err != nil {
+			// Assume this happened because the existing connection is no good.
+			// Just loop around now because we not only need a new connection, but
+			// also a new session.
+			continue
+		}
+		break
+	}
+
+	return &reader{
+		queueName:    queueName,
+		amqpSession:  amqpSession,
+		amqpReceiver: amqpReceiver,
+	}, nil
+}
+
+func (q *readerFactory) Close(context.Context) error {
+	if err := q.amqpClient.Close(); err != nil {
+		return errors.Wrapf(err, "error closing AMQP client")
+	}
+	log.Println("DEBUG: closed AMQP-based queue reader factory")
+	return nil
+}
+
+// reader is an AMQP-based implementation of the queue.Reader interface.
+type reader struct {
+	queueName    string
+	amqpSession  myamqp.Session
+	amqpReceiver myamqp.Receiver
+}
+
+func (q *reader) Read(
+	ctx context.Context,
+) (*queue.Message, error) {
+	amqpMsg, err := q.amqpReceiver.Receive(ctx)
+	if err != nil {
+		return nil, errors.Wrapf(
+			err,
+			"error receiving AMQP message for queue %q",
+			q.queueName,
+		)
+	}
+	return &queue.Message{
+		Message: string(amqpMsg.GetData()),
+		Ack:     amqpMsg.Accept,
+	}, nil
+}
+
+func (q *reader) Close(ctx context.Context) error {
+	if err := q.amqpReceiver.Close(ctx); err != nil {
+		return errors.Wrapf(
+			err,
+			"error closing AMQP receiver for queue %q",
+			q.queueName,
+		)
+	}
+	if err := q.amqpSession.Close(ctx); err != nil {
+		return errors.Wrapf(
+			err,
+			"error closing AMQP session for queue %q",
+			q.queueName,
+		)
+	}
+	return nil
+}
---
+++ v2/scheduler/internal/lib/queue/amqp/reader_test.go
@@ -0,0 +1,326 @@
+package amqp
+
+import (
+	"context"
+	"errors"
+	"os"
+	"sync"
+	"testing"
+
+	"github.com/Azure/go-amqp"
+	myamqp "github.com/brigadecore/brigade/v2/internal/amqp"
+	"github.com/brigadecore/brigade/v2/scheduler/internal/lib/queue"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetReaderFactoryConfig(t *testing.T) {
+	// Note that unit testing in Go does NOT clear environment variables between
+	// tests, which can sometimes be a pain, but it's fine here-- so each of these
+	// test cases builds on the previous case.
+	testCases := []struct {
+		name       string
+		setup      func()
+		assertions func(ReaderFactoryConfig, error)
+	}{
+		{
+			name:  "AMQP_ADDRESS not set",
+			setup: func() {},
+			assertions: func(_ ReaderFactoryConfig, err error) {
+				require.Error(t, err)
+				require.Contains(t, err.Error(), "value not found for")
+				require.Contains(t, err.Error(), "AMQP_ADDRESS")
+			},
+		},
+		{
+			name: "AMQP_USERNAME not set",
+			setup: func() {
+				os.Setenv("AMQP_ADDRESS", "foo")
+			},
+			assertions: func(_ ReaderFactoryConfig, err error) {
+				require.Error(t, err)
+				require.Contains(t, err.Error(), "value not found for")
+				require.Contains(t, err.Error(), "AMQP_USERNAME")
+			},
+		},
+		{
+			name: "AMQP_PASSWORD not set",
+			setup: func() {
+				os.Setenv("AMQP_USERNAME", "bar")
+			},
+			assertions: func(_ ReaderFactoryConfig, err error) {
+				require.Error(t, err)
+				require.Contains(t, err.Error(), "value not found for")
+				require.Contains(t, err.Error(), "AMQP_PASSWORD")
+			},
+		},
+		{
+			name: "SUCCESS not set",
+			setup: func() {
+				os.Setenv("AMQP_PASSWORD", "bat")
+			},
+			assertions: func(config ReaderFactoryConfig, err error) {
+				require.NoError(t, err)
+				require.Equal(
+					t,
+					ReaderFactoryConfig{
+						Address:  "foo",
+						Username: "bar",
+						Password: "bat",
+					},
+					config,
+				)
+			},
+		},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			testCase.setup()
+			config, err := GetReaderFactoryConfig()
+			testCase.assertions(config, err)
+		})
+	}
+}
+
+func TestNewReaderFactory(t *testing.T) {
+	const testAddress = "foo"
+	rf := NewReaderFactory(
+		ReaderFactoryConfig{
+			Address: testAddress,
+		},
+	)
+	require.IsType(t, &readerFactory{}, rf)
+	require.Equal(t, testAddress, rf.(*readerFactory).address)
+	require.NotEmpty(t, rf.(*readerFactory).dialOpts)
+	// Assert we're not connected yet. (It connects lazily.)
+	require.Nil(t, rf.(*readerFactory).amqpClient)
+	require.NotNil(t, rf.(*readerFactory).amqpClientMu)
+	require.NotNil(t, rf.(*readerFactory).connectFn)
+}
+
+func TestReaderFactoryNewReader(t *testing.T) {
+	const testQueueName = "foo"
+	rf := &readerFactory{
+		amqpClient: &mockAMQPClient{
+			NewSessionFn: func(opts ...amqp.SessionOption) (myamqp.Session, error) {
+				return &mockAMQPSession{
+					NewReceiverFn: func(
+						opts ...amqp.LinkOption,
+					) (myamqp.Receiver, error) {
+						return &mockAMQPReceiver{}, nil
+					},
+				}, nil
+			},
+		},
+		amqpClientMu: &sync.Mutex{},
+	}
+	w, err := rf.NewReader(testQueueName)
+	require.NoError(t, err)
+	require.IsType(t, &reader{}, w)
+	require.Equal(t, testQueueName, w.(*reader).queueName)
+	require.NotNil(t, w.(*reader).amqpSession)
+	require.NotNil(t, w.(*reader).amqpReceiver)
+}
+
+func TestReadFactoryClose(t *testing.T) {
+	testCases := []struct {
+		name          string
+		readerFactory queue.ReaderFactory
+		assertions    func(error)
+	}{
+		{
+			name: "error closing underlying amqp connection",
+			readerFactory: &readerFactory{
+				amqpClient: &mockAMQPClient{
+					CloseFn: func() error {
+						return errors.New("something went wrong")
+					},
+				},
+			},
+			assertions: func(err error) {
+				require.Error(t, err)
+				require.Contains(t, err.Error(), "something went wrong")
+				require.Contains(t, err.Error(), "error closing AMQP client")
+			},
+		},
+		{
+			name: "success",
+			readerFactory: &readerFactory{
+				amqpClient: &mockAMQPClient{
+					CloseFn: func() error {
+						return nil
+					},
+				},
+			},
+			assertions: func(err error) {
+				require.NoError(t, err)
+			},
+		},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			err := testCase.readerFactory.Close(context.Background())
+			testCase.assertions(err)
+		})
+	}
+}
+
+func TestReaderRead(t *testing.T) {
+	testCases := []struct {
+		name       string
+		reader     queue.Reader
+		assertions func(error)
+	}{
+		{
+			name: "error receiving message",
+			reader: &reader{
+				amqpReceiver: &mockAMQPReceiver{
+					ReceiveFn: func(context.Context) (*amqp.Message, error) {
+						return nil, errors.New("something went wrong")
+					},
+				},
+			},
+			assertions: func(err error) {
+				require.Error(t, err)
+				require.Contains(t, err.Error(), "something went wrong")
+				require.Contains(t, err.Error(), "error receiving AMQP message")
+			},
+		},
+		{
+			name: "success",
+			reader: &reader{
+				amqpReceiver: &mockAMQPReceiver{
+					ReceiveFn: func(context.Context) (*amqp.Message, error) {
+						return &amqp.Message{}, nil
+					},
+				},
+			},
+			assertions: func(err error) {
+				require.NoError(t, err)
+			},
+		},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			_, err := testCase.reader.Read(context.Background())
+			testCase.assertions(err)
+		})
+	}
+}
+
+func TestReaderClose(t *testing.T) {
+	testCases := []struct {
+		name       string
+		reader     queue.Reader
+		assertions func(error)
+	}{
+		{
+			name: "error closing underlying receiver",
+			reader: &reader{
+				amqpReceiver: &mockAMQPReceiver{
+					CloseFn: func(ctx context.Context) error {
+						return errors.New("something went wrong")
+					},
+				},
+			},
+			assertions: func(err error) {
+				require.Error(t, err)
+				require.Contains(t, err.Error(), "something went wrong")
+				require.Contains(t, err.Error(), "error closing AMQP receiver")
+			},
+		},
+		{
+			name: "error closing underlying session",
+			reader: &reader{
+				amqpReceiver: &mockAMQPReceiver{
+					CloseFn: func(ctx context.Context) error {
+						return nil
+					},
+				},
+				amqpSession: &mockAMQPSession{
+					CloseFn: func(ctx context.Context) error {
+						return errors.New("something went wrong")
+					},
+				},
+			},
+			assertions: func(err error) {
+				require.Error(t, err)
+				require.Contains(t, err.Error(), "something went wrong")
+				require.Contains(t, err.Error(), "error closing AMQP session")
+			},
+		},
+		{
+			name: "success",
+			reader: &reader{
+				amqpReceiver: &mockAMQPReceiver{
+					CloseFn: func(ctx context.Context) error {
+						return nil
+					},
+				},
+				amqpSession: &mockAMQPSession{
+					CloseFn: func(ctx context.Context) error {
+						return nil
+					},
+				},
+			},
+			assertions: func(err error) {
+				require.NoError(t, err)
+			},
+		},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			err := testCase.reader.Close(context.Background())
+			testCase.assertions(err)
+		})
+	}
+}
+
+type mockAMQPClient struct {
+	NewSessionFn func(opts ...amqp.SessionOption) (myamqp.Session, error)
+	CloseFn      func() error
+}
+
+func (m *mockAMQPClient) NewSession(
+	opts ...amqp.SessionOption,
+) (myamqp.Session, error) {
+	return m.NewSessionFn(opts...)
+}
+
+func (m *mockAMQPClient) Close() error {
+	return m.CloseFn()
+}
+
+type mockAMQPSession struct {
+	NewSenderFn   func(opts ...amqp.LinkOption) (myamqp.Sender, error)
+	NewReceiverFn func(opts ...amqp.LinkOption) (myamqp.Receiver, error)
+	CloseFn       func(ctx context.Context) error
+}
+
+func (m *mockAMQPSession) NewSender(
+	opts ...amqp.LinkOption,
+) (myamqp.Sender, error) {
+	return m.NewSenderFn(opts...)
+}
+
+func (m *mockAMQPSession) NewReceiver(
+	opts ...amqp.LinkOption,
+) (myamqp.Receiver, error) {
+	return m.NewReceiverFn(opts...)
+}
+
+func (m *mockAMQPSession) Close(ctx context.Context) error {
+	return m.CloseFn(ctx)
+}
+
+type mockAMQPReceiver struct {
+	ReceiveFn func(ctx context.Context) (*amqp.Message, error)
+	CloseFn   func(ctx context.Context) error
+}
+
+func (m *mockAMQPReceiver) Receive(ctx context.Context) (*amqp.Message, error) {
+	return m.ReceiveFn(ctx)
+}
+
+func (m *mockAMQPReceiver) Close(ctx context.Context) error {
+	return m.CloseFn(ctx)
+}
