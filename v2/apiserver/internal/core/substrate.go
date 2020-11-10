f0dc630115e20268320797a7bcd145b145b1efdd
Kent Rancourt <kent.rancourt@microsoft.com>
2020-11-10 14:59:20 -0500

    add substrate introspection endpoints to api server
    Signed-off-by: Kent Rancourt <kent.rancourt@microsoft.com>

--- v2/apiserver/internal/core/substrate.go
+++ v2/apiserver/internal/core/substrate.go
@@ -1,10 +1,116 @@
 package core
 
-import "context"
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/brigadecore/brigade/v2/apiserver/internal/meta"
+	"github.com/pkg/errors"
+)
+
+// SubstrateWorkerCount represents a count of Workers currently executing on
+// the substrate.
+type SubstrateWorkerCount struct {
+	// Count is the cardinality of Workers currently executing on the substrate.
+	Count int `json:"count"`
+}
+
+// MarshalJSON amends SubstrateWorkerCount instances with type metadata.
+func (s SubstrateWorkerCount) MarshalJSON() ([]byte, error) {
+	type Alias SubstrateWorkerCount
+	return json.Marshal(
+		struct {
+			meta.TypeMeta `json:",inline"`
+			Alias         `json:",inline"`
+		}{
+			TypeMeta: meta.TypeMeta{
+				APIVersion: meta.APIVersion,
+				Kind:       "SubstrateWorkerCount",
+			},
+			Alias: (Alias)(s),
+		},
+	)
+}
+
+// SubstrateJobCount represents a count of Workers currently executing on
+// the substrate.
+type SubstrateJobCount struct {
+	// Count is the cardinality of Jobs currently executing on the substrate.
+	Count int `json:"count"`
+}
+
+// MarshalJSON amends SubstrateJobCount instances with type metadata.
+func (s SubstrateJobCount) MarshalJSON() ([]byte, error) {
+	type Alias SubstrateJobCount
+	return json.Marshal(
+		struct {
+			meta.TypeMeta `json:",inline"`
+			Alias         `json:",inline"`
+		}{
+			TypeMeta: meta.TypeMeta{
+				APIVersion: meta.APIVersion,
+				Kind:       "SubstrateJobCount",
+			},
+			Alias: (Alias)(s),
+		},
+	)
+}
+
+// SubstrateService is the specialized interface for monitoring the state of the
+// substrate.
+type SubstrateService interface {
+	// CountRunningWorkers returns a count of Workers currently executing on the
+	// substrate.
+	CountRunningWorkers(context.Context) (SubstrateWorkerCount, error)
+	// CountRunningJobs returns a count of Jobs currently executing on the
+	// substrate.
+	CountRunningJobs(context.Context) (SubstrateJobCount, error)
+}
+
+type substrateService struct {
+	substrate Substrate
+}
+
+// NewSubstrateService returns a specialized interface for managing Projects.
+func NewSubstrateService(substrate Substrate) SubstrateService {
+	return &substrateService{
+		substrate: substrate,
+	}
+}
+
+func (s *substrateService) CountRunningWorkers(
+	ctx context.Context,
+) (SubstrateWorkerCount, error) {
+	count, err := s.substrate.CountRunningWorkers(ctx)
+	if err != nil {
+		return count, errors.Wrapf(
+			err,
+			"error counting running workers on substrate",
+		)
+	}
+	return count, nil
+}
+
+func (s *substrateService) CountRunningJobs(
+	ctx context.Context,
+) (SubstrateJobCount, error) {
+	count, err := s.substrate.CountRunningJobs(ctx)
+	if err != nil {
+		return count, errors.Wrapf(err, "error counting running jobs on substrate")
+	}
+	return count, nil
+}
 
 // Substrate is an interface for components that permit services to coordinate
 // with Brigade's underlying workload execution substrate, i.e. Kubernetes.
 type Substrate interface {
+	// CountRunningWorkers returns a count of Workers currently executing on the
+	// substrate.
+	CountRunningWorkers(context.Context) (SubstrateWorkerCount, error)
+	// CountRunningJobs returns a count of Jobs currently executing on the
+	// substrate.
+	CountRunningJobs(context.Context) (SubstrateJobCount, error)
+
 	// CreateProject prepares the substrate to host Project workloads. The
 	// provided Project argument may be amended with substrate-specific details
 	// and returned, so this function should be called prior to a Project being
