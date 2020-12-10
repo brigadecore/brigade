43d736647818bf722b3e1f45151a6ef13184e698
Kent Rancourt <kent.rancourt@microsoft.com>
2020-12-10 18:21:06 -0500

    misc. brigadier cleanup
    Signed-off-by: Kent Rancourt <kent.rancourt@microsoft.com>

--- v2/brigadier/src/index.ts
+++ v2/brigadier/src/index.ts
@@ -1,9 +1,4 @@
-import { Event, EventRegistry } from "./events"
-import { Group } from "./groups"
-import { Job, Container, JobHost } from "./jobs"
-import { logger } from "./logger"
-
-// events is the main event registry
-export const events = new EventRegistry()
-
-export { Event, Group, Job, Container, JobHost, logger }
+export { events } from "./events"
+export { Group } from "./groups"
+export { Job, Container, JobHost } from "./jobs"
+export { logger } from "./logger"
