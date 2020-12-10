9d523e2a8d395ff722de0a990b895bb2c469f2a0
Kent Rancourt <kent.rancourt@microsoft.com>
2020-12-10 16:55:05 -0500

    add logger component
    Signed-off-by: Kent Rancourt <kent.rancourt@microsoft.com>

--- v2/brigadier/src/index.ts
+++ v2/brigadier/src/index.ts
@@ -1,8 +1,9 @@
 import { Event, EventRegistry } from "./events"
 import { Group } from "./groups"
 import { Job, Container, JobHost } from "./jobs"
+import { logger } from "./logger"
 
 // events is the main event registry
 export const events = new EventRegistry()
 
-export { Event, Group, Job, Container, JobHost }
+export { Event, Group, Job, Container, JobHost, logger }
