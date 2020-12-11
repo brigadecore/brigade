b536a64509775abf065592b7d00cab3706d86485
Kent Rancourt <kent.rancourt@microsoft.com>
2020-12-11 13:24:48 -0500

    misc. brigadier improvements (#1193)
    * add logger component
    
    Signed-off-by: Kent Rancourt <kent.rancourt@microsoft.com>
    
    * add logLevel field
    
    Signed-off-by: Kent Rancourt <kent.rancourt@microsoft.com>
    
    * misc. brigadier cleanup
    
    Signed-off-by: Kent Rancourt <kent.rancourt@microsoft.com>
    
    * improve mock job
    
    Signed-off-by: Kent Rancourt <kent.rancourt@microsoft.com>
    
    * add more brigadier tests
    
    Signed-off-by: Kent Rancourt <kent.rancourt@microsoft.com>

---
+++ v2/brigadier/src/logger.ts
@@ -0,0 +1,23 @@
+import * as winston from "winston"
+import { TransformableInfo } from "logform"
+
+export { Logger } from "winston" 
+
+export const logger = winston.createLogger({
+  levels: {
+    error: 0,
+    warn: 1,
+    info: 2,
+    debug: 3
+  },
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.printf((info: TransformableInfo) => {
+      if (info.job) {
+        return `${info.timestamp} [job: ${info.job}] ${info.level.toUpperCase()}: ${info.message}`
+      }
+      return `${info.timestamp} ${info.level.toUpperCase()}: ${info.message}`
+    })
+  ),
+  transports: [ new winston.transports.Console() ]
+})
