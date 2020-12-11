611daf58a71a38b8c291defa305db9dd6cdf2739
Kent Rancourt <kent.rancourt@microsoft.com>
2020-12-11 10:51:38 -0500

    set and fine tune context timeout on api calls from observer
    Signed-off-by: Kent Rancourt <kent.rancourt@microsoft.com>

--- v2/observer/common.go
+++ v2/observer/common.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"time"
+
 	corev1 "k8s.io/api/core/v1"
 )
+
+const apiRequestTimeout = 30 * time.Second
 
 // syncDeletedPod only fires when a pod deletion is COMPLETE. i.e. The pod is
 // completely gone.
