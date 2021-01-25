db186f317dbff71ae3a227ff64d9c5659cadb07b
Kent Rancourt <kent.rancourt@microsoft.com>
2021-01-25 18:55:27 -0500

    DRY up some assertion helpers in sdk (#1248)
    Signed-off-by: Kent Rancourt <kent.rancourt@microsoft.com>

--- sdk/v2/core/workers_test.go
+++ sdk/v2/core/workers_test.go
@@ -10,17 +10,23 @@ import (
 	"testing"
 	"time"
 
+	rmTesting "github.com/brigadecore/brigade/sdk/v2/internal/restmachinery/testing" // nolint: lll
+	metaTesting "github.com/brigadecore/brigade/sdk/v2/meta/testing"
 	"github.com/stretchr/testify/require"
 )
 
 func TestWorkerStatusMarshalJSON(t *testing.T) {
-	requireAPIVersionAndType(t, WorkerStatus{}, "WorkerStatus")
+	metaTesting.RequireAPIVersionAndType(t, WorkerStatus{}, "WorkerStatus")
 }
 
 func TestNewWorkersClient(t *testing.T) {
-	client := NewWorkersClient(testAPIAddress, testAPIToken, nil)
+	client := NewWorkersClient(
+		rmTesting.TestAPIAddress,
+		rmTesting.TestAPIToken,
+		nil,
+	)
 	require.IsType(t, &workersClient{}, client)
-	requireBaseClient(t, client.(*workersClient).BaseClient)
+	rmTesting.RequireBaseClient(t, client.(*workersClient).BaseClient)
 	require.NotNil(t, client.(*workersClient).jobsClient)
 	require.Equal(t, client.(*workersClient).jobsClient, client.Jobs())
 }
@@ -41,7 +47,7 @@ func TestWorkersClientStart(t *testing.T) {
 		),
 	)
 	defer server.Close()
-	client := NewWorkersClient(server.URL, testAPIToken, nil)
+	client := NewWorkersClient(server.URL, rmTesting.TestAPIToken, nil)
 	err := client.Start(context.Background(), testEventID)
 	require.NoError(t, err)
 }
@@ -68,7 +74,7 @@ func TestWorkersClientGetStatus(t *testing.T) {
 		),
 	)
 	defer server.Close()
-	client := NewWorkersClient(server.URL, testAPIToken, nil)
+	client := NewWorkersClient(server.URL, rmTesting.TestAPIToken, nil)
 	workerStatus, err := client.GetStatus(context.Background(), testEventID)
 	require.NoError(t, err)
 	require.Equal(t, testWorkerStatus, workerStatus)
@@ -103,7 +109,7 @@ func TestWorkersClientWatchStatus(t *testing.T) {
 		),
 	)
 	defer server.Close()
-	client := NewWorkersClient(server.URL, testAPIToken, nil)
+	client := NewWorkersClient(server.URL, rmTesting.TestAPIToken, nil)
 	statusCh, _, err := client.WatchStatus(context.Background(), testEventID)
 	require.NoError(t, err)
 	select {
@@ -141,7 +147,7 @@ func TestWorkersClientUpdateStatus(t *testing.T) {
 		),
 	)
 	defer server.Close()
-	client := NewWorkersClient(server.URL, testAPIToken, nil)
+	client := NewWorkersClient(server.URL, rmTesting.TestAPIToken, nil)
 	err := client.UpdateStatus(
 		context.Background(),
 		testEventID,
@@ -168,7 +174,7 @@ func TestWorkersClientCleanup(t *testing.T) {
 		),
 	)
 	defer server.Close()
-	client := NewWorkersClient(server.URL, testAPIToken, nil)
+	client := NewWorkersClient(server.URL, rmTesting.TestAPIToken, nil)
 	err := client.Cleanup(context.Background(), testEventID)
 	require.NoError(t, err)
 }
