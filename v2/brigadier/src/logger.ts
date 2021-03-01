cc9cdc69813bfb50fcf30c74e0d59e4ca3b8993f
itowlson <ivan.towlson@microsoft.com>
2021-03-02 09:49:07 +1300

    Logger docs
    Signed-off-by: itowlson <ivan.towlson@microsoft.com>

--- v2/brigadier/src/logger.ts
+++ v2/brigadier/src/logger.ts
@@ -1,20 +1,47 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+/**
+ * Provides logging services for a script.
+ * 
+ * Access the logger through the global `logger` object.
+ */
 class Logger {
 
+  /**
+   * Logs a message at Debug level.
+   * @param message The message to log
+   * @param meta Values to replace any substitution strings in `message`
+   */
   public debug(message: string, ...meta: any[]): Logger {
     console.debug(message, ...meta)
     return this
   }
 
+  /**
+   * Logs a message at Information level.
+   * @param message The message to log
+   * @param meta Values to replace any substitution strings in `message`
+   */
   public info(message: string, ...meta: any[]): Logger {
     console.info(message, ...meta)
     return this
   }
 
+  /**
+   * Logs a message at Warning level.
+   * @param message The message to log
+   * @param meta Values to replace any substitution strings in `message`
+   */
   public warn(message: string, ...meta: any[]): Logger {
     console.warn(message, ...meta)
     return this
   }
 
+  /**
+   * Logs a message at Error level.
+   * @param message The message to log
+   * @param meta Values to replace any substitution strings in `message`
+   */
   public error(message: string, ...meta: any[]): Logger {
     console.error(message, ...meta)
     return this
@@ -22,4 +49,5 @@ class Logger {
 
 }
 
+/** Provides logging for a script. */
 export const logger = new Logger()
