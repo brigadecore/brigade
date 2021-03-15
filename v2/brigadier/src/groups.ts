b135874c6547935e27ff0444e6ffd2aa66edec4c
Kent Rancourt <kent.rancourt@microsoft.com>
2021-03-14 20:42:44 -0400

    Composable execution interface in Brigadier (#1292)
    * add Runnable interface; divide Groups into SerialGroup and ConcurrentGroup
    
    Signed-off-by: Kent Rancourt <kent.rancourt@microsoft.com>
    Signed-off-by: itowlson <ivan.towlson@microsoft.com>
    
    * add group examples
    
    Signed-off-by: Kent Rancourt <kent.rancourt@microsoft.com>
    Signed-off-by: itowlson <ivan.towlson@microsoft.com>
    
    * Make all runnables discoverable from Job methods
    
    Signed-off-by: Ivan Towlson <github@hestia.cc>
    Signed-off-by: itowlson <ivan.towlson@microsoft.com>
    
    * Partial JSDoc (needs more work)
    
    Signed-off-by: itowlson <ivan.towlson@microsoft.com>
    
    * Moar JSDoc
    
    Signed-off-by: itowlson <ivan.towlson@microsoft.com>
    
    * And more and more JSDoc
    
    Signed-off-by: itowlson <ivan.towlson@microsoft.com>
    
    * Logger docs
    
    Signed-off-by: itowlson <ivan.towlson@microsoft.com>
    
    * Documentation improvements from review
    
    Signed-off-by: itowlson <ivan.towlson@microsoft.com>
    
    * feat(restmachinery/client.go): use custom retry policy to exclude 500 from retries (#1314)
    
    Signed-off-by: Vaughn Dice <vadice@microsoft.com>
    Signed-off-by: itowlson <ivan.towlson@microsoft.com>
    
    Co-authored-by: Ivan Towlson <github@hestia.cc>
    Co-authored-by: itowlson <ivan.towlson@microsoft.com>
    Co-authored-by: Vaughn Dice <vadice@microsoft.com>

--- v2/brigadier/src/groups.ts
+++ v2/brigadier/src/groups.ts
@@ -1,45 +1,93 @@
-import { Job } from "./jobs"
+import { Runnable } from "./runnables"
 
-export class Group {
+/**
+ * The base type for Runnables composed of other Runnables.
+ * Do not construct the base Group type; use Job.sequential (or SerialGroup)
+ * or Job.concurrent (or ConcurrentGroup) instead.
+ * 
+ * @abstract
+ */
+class Group {
+  protected runnables: Runnable[]
 
-  public static async runAll(jobs: Job[]): Promise<void[]> {
-    const group = new Group(jobs)
-    return group.runAll()
+  protected constructor(...runnables: Runnable[]) {
+    this.runnables = runnables || []
   }
 
-  public static async runEach(jobs: Job[]): Promise<void> {
-    const group = new Group(jobs)
-    return group.runEach()
-  }
-
-  private jobs: Job[]
-
-  public constructor(jobs?: Job[]) {
-    this.jobs = jobs || []
-  }
-
-  public add(...jobs: Job[]): void {
-    for (const job of jobs) {
-      this.jobs.push(job)
+  public add(...runnables: Runnable[]): void {
+    for (const runnable of runnables) {
+      this.runnables.push(runnable)
     }
   }
 
   public length(): number {
-    return this.jobs.length
+    return this.runnables.length
+  }
+}
+
+/**
+ * A Runnable composed of sub-Runnables (such as jobs
+ * or concurrent groups) running one after another.
+ * A new Runnable is started only when the previous one completes.
+ * The sequence completes when the last Runnable has completed (or when any
+ * Runnable fails).
+ * 
+ * @param runnables The work items to be run in sequence
+ */
+export class SerialGroup extends Group implements Runnable {
+  public constructor(...runnables: Runnable[]) {
+    super(...runnables)
   }
 
-  public async runEach(): Promise<void> {
-    for (const job of this.jobs) {
-      await job.run()
+  /**
+   * Runs the serial group.
+   * 
+   * @returns A Promise which completes when the last item in the group completes (or
+   * any item fails). If all items ran successfully, the Promise resolves; if any
+   * item failed (that is, its Runnable#run Promise rejected), the Promise
+   * rejects.
+   */
+  public async run(): Promise<void> {
+    for (const runnable of this.runnables) {
+      await runnable.run()
     }
   }
 
-  public async runAll(): Promise<void[]> {
+}
+
+/**
+ * A Runnable composed of sub-Runnables (such as jobs
+ * or sequential groups) running concurrently.
+ * When run, all Runnables are started simultaneously (subject to
+ * scheduling constraints).
+ * The concurrent group completes when all Runnables have completed.
+ * 
+ * @param runnables The work items to be run in parallel
+ */
+export class ConcurrentGroup extends Group implements Runnable {
+  public constructor(...runnables: Runnable[]) {
+    super(...runnables)
+  }
+
+  /**
+   * Runs the concurrent group.
+   * 
+   * @returns A Promise which completes when all items in the group complete.
+   * If all items ran successfully, the Promise resolves; if any
+   * item failed (that is, its Runnable#run Promise rejected), the Promise
+   * rejects.
+   */
+  public async run(): Promise<void> {
     const promises: Promise<void>[] = []
-    for (const job of this.jobs) {
-      promises.push(job.run())
+    for (const runnable of this.runnables) {
+      promises.push(runnable.run())
     }
-    return Promise.all(promises)
+    try {
+      await Promise.all(promises)
+      return Promise.resolve()
+    } catch(e) {
+      return Promise.reject(e)
+    }
   }
 
 }
--- v2/brigadier/src/projects.ts
+++ v2/brigadier/src/projects.ts
@@ -1,4 +1,9 @@
+/**
+ * Provides details of a Brigade project that triggered an event.
+ */
 export interface Project {
+  /** The unique identifier of the project. */
   id: string
+  /** A map of secrets defined in the Brigade project. */
   secrets: { [key: string]: string }
 }
