1140a13e36de39ea1a77380401ff0cd355f7aff8
Kent Rancourt <kent.rancourt@microsoft.com>
2021-03-18 14:25:37 -0400

    export Event from brigadier and worker's brigadier polyfill
    Signed-off-by: Kent Rancourt <kent.rancourt@microsoft.com>

--- v2/brigadier/test/index.ts
+++ v2/brigadier/test/index.ts
@@ -6,11 +6,13 @@ import * as brigadier from "../src"
 describe("brigadier", () => {
   it("has expected exports", () => {
     assert.property(brigadier, "Container")
-    assert.property(brigadier, "events")
-    assert.property(brigadier, "SerialGroup")
     assert.property(brigadier, "ConcurrentGroup")
+    // TODO: Figure out how to assert that an interface was exported.
+    // assert.property(brigadier, "Event")
+    assert.property(brigadier, "events")
     assert.property(brigadier, "Job")    
     assert.property(brigadier, "JobHost")
     assert.property(brigadier, "logger")
+    assert.property(brigadier, "SerialGroup")
   })
 })
