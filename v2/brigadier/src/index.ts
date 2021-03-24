a1e2a93604eb21fa2ca718d097670f9bcf0e8b35
Kent Rancourt <kent.rancourt@microsoft.com>
2021-03-24 12:56:13 -0400

    export EventRegistry from brigadier
    Signed-off-by: Kent Rancourt <kent.rancourt@microsoft.com>

--- v2/brigadier/src/index.ts
+++ v2/brigadier/src/index.ts
@@ -1,4 +1,4 @@
-export { Event, events } from "./events"
+export { Event, EventRegistry, events } from "./events"
 export { ConcurrentGroup, SerialGroup } from "./groups"
 export { Container, Job, JobHost } from "./jobs"
 export { logger } from "./logger"
