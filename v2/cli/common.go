8357e68685419d1a50631e20ef50b02696119376
Kent Rancourt <kent.rancourt@microsoft.com>
2021-03-30 14:47:26 -0400

    add --non-interactive and --continue flags to CLI (#1334)
    Signed-off-by: Kent Rancourt <kent.rancourt@microsoft.com>

--- v2/cli/common.go
+++ v2/cli/common.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/AlecAivazis/survey/v2"
 	"github.com/pkg/errors"
 	"github.com/urfave/cli/v2"
+	"golang.org/x/crypto/ssh/terminal"
 )
 
 // confirmed prompts the user to confirm an irreversible action and returns a
@@ -15,6 +18,15 @@ func confirmed(c *cli.Context) (bool, error) {
 	if confirmed {
 		return true, nil
 	}
+
+	if c.Bool(flagNonInteractive) || !terminal.IsTerminal(int(os.Stdout.Fd())) {
+		return false,
+			errors.New(
+				"In non-interactive mode, this action must be confirmed with the " +
+					"--yes flag",
+			)
+	}
+
 	if err := survey.AskOne(
 		&survey.Confirm{
 			Message: "This action cannot be undone. Are you sure?",
@@ -30,7 +42,28 @@ func confirmed(c *cli.Context) (bool, error) {
 // shouldContinue prompts the user to indicate whether additional query results
 // should be fetched using another API request and returns a bool indicating yes
 // (true) or no (false).
-func shouldContinue(remainingItemCount int64) (bool, error) {
+func shouldContinue(
+	c *cli.Context,
+	remainingItemCount int64,
+	continueVal string,
+) (bool, error) {
+	outputFormat := strings.ToLower(c.String(flagOutput))
+
+	if remainingItemCount < 1 || continueVal == "" || outputFormat != "table" {
+		return false, nil
+	}
+
+	// If running with --non-interactive or this isn't a terminal, tell the
+	// user how to get the next page of results.
+	if c.Bool(flagNonInteractive) || !terminal.IsTerminal(int(os.Stdout.Fd())) {
+		fmt.Printf(
+			"\n%d results remain. Fetch the next page using --continue %s\n",
+			remainingItemCount,
+			continueVal,
+		)
+		return false, nil
+	}
+
 	var shouldContinue bool
 	fmt.Println()
 	if err := survey.AskOne(
