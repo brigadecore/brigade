73556efe3ebaac05bc3c14786464b9a36546a50f
Kent Rancourt <kent.rancourt@microsoft.com>
2021-04-29 14:14:19 -0400

    corresponding sdk changes
    Signed-off-by: Kent Rancourt <kent.rancourt@microsoft.com>

--- sdk/v2/system/named_roles.go
+++ sdk/v2/system/named_roles.go
@@ -8,6 +8,17 @@ const (
 	// ServiceAccounts.
 	RoleAdmin libAuthz.Role = "ADMIN"
 
+	// RoleEventCreator is the name of a system-level Role that enables principals
+	// to create Events for all Projects-- provided the Events have a specific
+	// value in the Source field. This is useful for Event gateways, which should
+	// be able to create Events for all Projects, but should NOT be able to
+	// impersonate other gateways.
+	RoleEventCreator libAuthz.Role = "EVENT_CREATOR"
+
+	// RoleProjectCreator is the name of a system-level Role that enables
+	// principals to create new Projects.
+	RoleProjectCreator libAuthz.Role = "PROJECT_CREATOR"
+
 	// RoleReader is the name of a system-level Role that enables global read
 	// access.
 	RoleReader libAuthz.Role = "READER"
