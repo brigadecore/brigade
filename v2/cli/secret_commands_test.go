cf483921220196f7cab608e1bd2cda15814d7910
Kent Rancourt <kent.rancourt@microsoft.com>
2021-05-05 16:23:56 -0400

    more improvements to secret parsing (#1397)
    Signed-off-by: Kent Rancourt <kent.rancourt@microsoft.com>

---
+++ v2/cli/secret_commands_test.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestResolveEnvVars(t *testing.T) {
+	testCases := []struct {
+		name           string
+		setup          func()
+		input          string
+		expectedResult string
+	}{
+		{
+			name:           "no substitutions",
+			input:          "foobar",
+			expectedResult: "foobar",
+		},
+		{
+			name:           "failed substitution",
+			input:          "${SUB}bar",
+			expectedResult: "bar",
+		},
+		{
+			name: "one substitution",
+			setup: func() {
+				os.Setenv("SUB", "foo")
+			},
+			input:          "${SUB}bar",
+			expectedResult: "foobar",
+		},
+		{
+			name: "multiple substitutions",
+			setup: func() {
+				os.Setenv("SUB1", "foo")
+				os.Setenv("SUB2", "bar")
+			},
+			input:          "${SUB1}${SUB2}",
+			expectedResult: "foobar",
+		},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			if testCase.setup != nil {
+				testCase.setup()
+			}
+			require.Equal(t, testCase.expectedResult, resolveEnvVars(testCase.input))
+		})
+	}
+}
