a5a1a99f21deb64e73466ed2a74fa26d5c29acc5
Vaughn Dice <vadice@microsoft.com>
2021-06-16 11:34:56 -0600

    feat(polyfill/jobs.ts): update Job.wait() to use SDK
    Signed-off-by: Vaughn Dice <vadice@microsoft.com>

--- v2/brigadier-polyfill/src/jobs.ts
+++ v2/brigadier-polyfill/src/jobs.ts
@@ -45,68 +45,54 @@ export class Job extends BrigadierJob {
 
   private async wait(): Promise<void> {
     return new Promise<void>((resolve, reject) => {
-      const eventSource = new EventSource(
-        `${this.event.worker.apiAddress}/v2/events/${this.event.id}/worker/jobs/${this.name}/status?watch=true&sse=true`, 
-        {
-          https: {
-            // TODO: Get our hands on the API server's CA to validate the cert
-            rejectUnauthorized: false
-          },
-          headers: {
-            "Authorization": `Bearer ${this.event.worker.apiToken}`
+      try {
+        const jobsClient = new core.JobsClient(
+          this.event.worker.apiAddress,
+          this.event.worker.apiToken,
+          {allowInsecureConnections: true},
+        )
+
+        const statusStream = jobsClient.watchStatus(this.event.id, this.name)
+        statusStream.onData((status: core.JobStatus) => {
+          this.logger.debug(`Current job phase is ${status.phase}`)
+          switch (status.phase) {
+          case core.JobPhase.Aborted:
+            reject(new Error(`Job "${this.name}" was aborted`))
+            break
+          // TODO: uncomment once SDK has core.JobPhase.Canceled
+          // case core.JobPhase.Canceled:
+          //   reject(new Error(`Job "${this.name}" was canceled before starting`))
+          //   break
+          case core.JobPhase.Failed:
+            reject(new Error(`Job "${this.name}" failed`))
+            break
+          case core.JobPhase.SchedulingFailed:
+            reject(new Error(`Job "${this.name}" scheduling failed`))
+            break
+          case core.JobPhase.Succeeded:
+            resolve()
+            break
+          case core.JobPhase.TimedOut:
+            reject(new Error(`Job "${this.name}" timed out`))
+            break
           }
-        }
-      )
-      eventSource.addEventListener("message", (event: any) => { // eslint-disable-line @typescript-eslint/no-explicit-any
-        let status: any // eslint-disable-line @typescript-eslint/no-explicit-any
-        try {
-          status = JSON.parse(event.data)
-        } catch(e) {
-          eventSource.close() 
-          reject(new Error(`Error parsing job "${this.name}" status: ${e.message}`))
-        }
-        this.logger.debug(`Current job phase is ${status.phase}`)
-        switch (status.phase) {
-        case "ABORTED":
-          eventSource.close()
-          reject(new Error(`Job "${this.name}" was aborted`))
-          break
-        case "CANCELED":
-          eventSource.close()
-          reject(new Error(`Job "${this.name}" was canceled before starting`))
-          break
-        case "FAILED":
-          eventSource.close()
-          reject(new Error(`Job "${this.name}" failed`))
-          break
-        case "SCHEDULING_FAILED":
-          eventSource.close()
-          reject(new Error(`Job "${this.name}" scheduling failed`))
-          break
-        case "SUCCEEDED":
-          eventSource.close()
+        })
+        statusStream.onReconnecting(() => {
+          console.log("status stream connecting")
+        })
+        statusStream.onClosed(() => {
+          reject("status stream closed")
+        })
+        statusStream.onError((e: Error) => {
+          reject(e)
+        })
+        statusStream.onDone(() => {
           resolve()
-          break
-        case "TIMED_OUT":
-          eventSource.close()
-          reject(new Error(`Job "${this.name}" timed out`))
-          break
-        }
-        // For all other phases there's nothing to do. Keep waiting.
-      })
-      eventSource.addEventListener("error", (e: any) => { // eslint-disable-line @typescript-eslint/no-explicit-any
-        if (e.status) { // If the error has an HTTP status code associated with it...
-          eventSource.close()
-          reject(new Error(`Received ${e.status} from the API server when attempting to open job "${this.name}" status stream`))
-        } else if (eventSource.readyState == EventSource.CONNECTING) {
-          // We lost the connection and we're reconnecting... nbd
-          this.logger.debug("Reconnecting to status stream")
-        } else if (eventSource.readyState == EventSource.CLOSED) {
-          // We disconnected for some unknown reason... and presumably exhausted
-          // attempts to reconnect
-          reject(new Error(`Error receiving job "${this.name}" status stream: ${e.message}`))
-        }
-      })
+        })
+      }
+      catch(e) {
+        throw new Error(`Error watching status for job "${this.name}": ${e.message}`)
+      }
     })
   }
 
