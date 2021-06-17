6e75425140abd73d25e35c30a30433e26fe34593
Vaughn Dice <vadice@microsoft.com>
2021-06-17 13:43:37 -0600

    ref(brigadier-polyfill): finish updating jobs.ts to use sdk (#1451)
    Signed-off-by: Vaughn Dice <vadice@microsoft.com>

--- v2/brigadier-polyfill/src/jobs.ts
+++ v2/brigadier-polyfill/src/jobs.ts
@@ -1,6 +1,3 @@
-// For some reason, EventSource NEEDS to be required this way.
-const EventSource = require("eventsource") // eslint-disable-line @typescript-eslint/no-var-requires
-
 import { Logger } from "winston" 
 
 import { Event, Job as BrigadierJob } from "@brigadecore/brigadier"
@@ -45,117 +42,86 @@ export class Job extends BrigadierJob {
 
   private async wait(): Promise<void> {
     return new Promise<void>((resolve, reject) => {
-      const eventSource = new EventSource(
-        `${this.event.worker.apiAddress}/v2/events/${this.event.id}/worker/jobs/${this.name}/status?watch=true&sse=true`, 
-        {
-          https: {
-            // TODO: Get our hands on the API server's CA to validate the cert
-            rejectUnauthorized: false
-          },
-          headers: {
-            "Authorization": `Bearer ${this.event.worker.apiToken}`
-          }
-        }
+      const jobsClient = new core.JobsClient(
+        this.event.worker.apiAddress,
+        this.event.worker.apiToken,
+        {allowInsecureConnections: true},
       )
-      eventSource.addEventListener("message", (event: any) => { // eslint-disable-line @typescript-eslint/no-explicit-any
-        let status: any // eslint-disable-line @typescript-eslint/no-explicit-any
-        try {
-          status = JSON.parse(event.data)
-        } catch(e) {
-          eventSource.close() 
-          reject(new Error(`Error parsing job "${this.name}" status: ${e.message}`))
-        }
+
+      const statusStream = jobsClient.watchStatus(this.event.id, this.name)
+      statusStream.onData((status: core.JobStatus) => {
         this.logger.debug(`Current job phase is ${status.phase}`)
         switch (status.phase) {
-        case "ABORTED":
-          eventSource.close()
+        case core.JobPhase.Aborted:
           reject(new Error(`Job "${this.name}" was aborted`))
           break
-        case "CANCELED":
-          eventSource.close()
+        case core.JobPhase.Canceled:
           reject(new Error(`Job "${this.name}" was canceled before starting`))
           break
-        case "FAILED":
-          eventSource.close()
+        case core.JobPhase.Failed:
           reject(new Error(`Job "${this.name}" failed`))
           break
-        case "SCHEDULING_FAILED":
-          eventSource.close()
+        case core.JobPhase.SchedulingFailed:
           reject(new Error(`Job "${this.name}" scheduling failed`))
           break
-        case "SUCCEEDED":
-          eventSource.close()
+        case core.JobPhase.Succeeded:
           resolve()
           break
-        case "TIMED_OUT":
-          eventSource.close()
+        case core.JobPhase.TimedOut:
           reject(new Error(`Job "${this.name}" timed out`))
           break
         }
-        // For all other phases there's nothing to do. Keep waiting.
       })
-      eventSource.addEventListener("error", (e: any) => { // eslint-disable-line @typescript-eslint/no-explicit-any
-        if (e.status) { // If the error has an HTTP status code associated with it...
-          eventSource.close()
-          reject(new Error(`Received ${e.status} from the API server when attempting to open job "${this.name}" status stream`))
-        } else if (eventSource.readyState == EventSource.CONNECTING) {
-          // We lost the connection and we're reconnecting... nbd
-          this.logger.debug("Reconnecting to status stream")
-        } else if (eventSource.readyState == EventSource.CLOSED) {
-          // We disconnected for some unknown reason... and presumably exhausted
-          // attempts to reconnect
-          reject(new Error(`Error receiving job "${this.name}" status stream: ${e.message}`))
-        }
+      statusStream.onReconnecting(() => {
+        console.log("status stream connecting")
+      })
+      statusStream.onClosed(() => {
+        reject("status stream closed")
+      })
+      statusStream.onError((e: Error) => {
+        reject(new Error(`Error watching status for job "${this.name}": ${e.message}`))
+      })
+      statusStream.onDone(() => {
+        resolve()
       })
     })
   }
 
   async logs(): Promise<string> {
     return new Promise<string>((resolve, reject) => {
-      const eventSource = new EventSource(
-        `${this.event.worker.apiAddress}/v2/events/${this.event.id}/logs?job=${this.name}&sse=true`, 
-        {
-          https: {
-            // TODO: Get our hands on the API server's CA to validate the cert
-            rejectUnauthorized: false
-          },
-          headers: {
-            "Authorization": `Bearer ${this.event.worker.apiToken}`
-          }
-        }
+      const logsClient = new core.LogsClient(
+        this.event.worker.apiAddress,
+        this.event.worker.apiToken,
+        {allowInsecureConnections: true},
+      )
+
+      const logsStream = logsClient.stream(
+        this.event.id,
+        {job: this.name},
+        {follow: false},
       )
       let logs = ""
-      eventSource.addEventListener("message", (event: any) => { // eslint-disable-line @typescript-eslint/no-explicit-any
-        let logEntry: any // eslint-disable-line @typescript-eslint/no-explicit-any
-        try {
-          logEntry = JSON.parse(event.data)
-        } catch(e) {
-          eventSource.close() 
-          reject(new Error(`Error parsing log entry for job "${this.name}": ${e.message}`))
-        }
+      logsStream.onData((logEntry: core.LogEntry) => {
         if (logs != "") {
           logs += "\n"
         }
+        if (logEntry.time) {
+          logs += logEntry.time + ": "
+        }
         logs += logEntry.message
       })
-      eventSource.addEventListener("error", (e: any) => { // eslint-disable-line @typescript-eslint/no-explicit-any
-        if (e.status) { // If the error has an HTTP status code associated with it...
-          eventSource.close()
-          reject(new Error(`Received ${e.status} from the API server when attempting to open job "${this.name}" log stream`))
-        } else if (eventSource.readyState == EventSource.CONNECTING) {
-          // We lost the connection and we're reconnecting... nbd
-          this.logger.debug("Reconnecting to log stream")
-        } else if (eventSource.readyState == EventSource.CLOSED) {
-          // We disconnected for some unknown reason... and presumably exhausted
-          // attempts to reconnect
-          reject(new Error(`Encountered unknown error receiving job "${this.name}" log stream`))
-        }
+      logsStream.onReconnecting(() => {
+        console.log("log stream connecting")
       })
-      eventSource.addEventListener("done", () => {
-        eventSource.close()
+      logsStream.onClosed(() => {
+        reject("log stream closed")
+      })
+      logsStream.onError((e: Error) => {
+        reject(new Error(`Error retrieving logs for job "${this.name}": ${e.message}`))
+      })
+      logsStream.onDone(() => {
         resolve(logs)
       })
     })
   }
-
 }
