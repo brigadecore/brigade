2bf0a0a9a5aaa22b8965f11d7547084071ef8b4a
Kent Rancourt <kent.rancourt@microsoft.com>
2021-10-13 13:17:53 -0400

    brigadier: export ImagePullPolicy
    Signed-off-by: Kent Rancourt <kent.rancourt@microsoft.com>

--- v2/brigadier/src/index.ts
+++ v2/brigadier/src/index.ts
@@ -1,4 +1,4 @@
 export { Event, EventRegistry, events } from "./events"
 export { ConcurrentGroup, SerialGroup } from "./groups"
-export { Container, Job, JobHost } from "./jobs"
+export { Container, ImagePullPolicy, Job, JobHost } from "./jobs"
 export { logger } from "./logger"
