9d079cecc64b08e0793ceddb4330428ef316daef
Prajwal S N <prajwalnadig21@gmail.com>
2021-10-26 23:31:02 +0530

    cli: added server version
    Signed-off-by: Prajwal S N <prajwalnadig21@gmail.com>

--- v2/cli/version_command.go
+++ v2/cli/version_command.go
@@ -1,20 +1,51 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 
 	"github.com/brigadecore/brigade-foundations/version"
 	"github.com/urfave/cli/v2"
 )
 
 var versionCommand = &cli.Command{
-	Name:   "version",
-	Usage:  "Print Brigade version",
+	Name:  "version",
+	Usage: "Print Brigade version",
+	Flags: []cli.Flag{
+		&cli.BoolFlag{
+			Name:    flagClient,
+			Aliases: []string{"c"},
+			Usage:   "Prints only the Brigade client version",
+		},
+	},
 	Action: printVersion,
 }
 
 func printVersion(c *cli.Context) error {
+	if c.Bool(flagClient) {
+		printClientVersion()
+	} else {
+		printClientVersion()
+		printServerVersion()
+	}
+	return nil
+}
+
+func printClientVersion() {
 	fmt.Printf("Brigade version %s -- commit %s\n",
 		version.Version(), version.Commit())
-	return nil
+}
+
+func printServerVersion() {
+	client, err := getClient()
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx := context.Background()
+	serverVersionRaw, err := client.System().UnversionedPing(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Brigade API Server version %s", string(serverVersionRaw))
 }
