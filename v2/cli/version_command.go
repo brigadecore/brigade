6e6b4ecfe0c538ccca9c5715fbbaed7e07621ebd
Prajwal S N <prajwalnadig21@gmail.com>
2021-10-27 21:50:33 +0530

    Print server version with `brig version` (#1693)
    * bug: moved `defer` after error handling
    
    Signed-off-by: Prajwal S N <prajwalnadig21@gmail.com>
    
    * cli: added server version
    
    Signed-off-by: Prajwal S N <prajwalnadig21@gmail.com>
    
    * cli(version): repaired error handling
    
    Signed-off-by: Prajwal S N <prajwalnadig21@gmail.com>
    
    * cli(version): better error handling
    
    Signed-off-by: Prajwal S N <prajwalnadig21@gmail.com>
    
    * cli(version): add review changes
    
    Signed-off-by: Prajwal S N <prajwalnadig21@gmail.com>
    
    Co-authored-by: Kent Rancourt <kent.rancourt@microsoft.com>

--- v2/cli/version_command.go
+++ v2/cli/version_command.go
@@ -8,13 +8,33 @@ import (
 )
 
 var versionCommand = &cli.Command{
-	Name:   "version",
-	Usage:  "Print Brigade version",
+	Name:  "version",
+	Usage: "Print Brigade version",
+	Flags: []cli.Flag{
+		&cli.BoolFlag{
+			Name:    flagClient,
+			Aliases: []string{"c"},
+			Usage:   "Prints only the Brigade client version",
+		},
+	},
 	Action: printVersion,
 }
 
 func printVersion(c *cli.Context) error {
+	// Client version
 	fmt.Printf("Brigade version %s -- commit %s\n",
 		version.Version(), version.Commit())
+	// Server version
+	if !c.Bool(flagClient) {
+		client, err := getClient()
+		if err != nil {
+			return err
+		}
+		serverVersionRaw, err := client.System().UnversionedPing(c.Context)
+		if err != nil {
+			return err
+		}
+		fmt.Printf("Brigade API Server version %s", string(serverVersionRaw))
+	}
 	return nil
 }
