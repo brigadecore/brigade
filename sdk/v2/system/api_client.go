3b2717808c7ef8c48e1bd74a20433f80e90e93c2
Prajwal S N <prajwalnadig21@gmail.com>
2021-10-27 21:50:33 +0530

    Print server version with `brig version` (#1693)
    * bug: moved `defer` after error handling
    
    Signed-off-by: Prajwal S N <prajwalnadig21@gmail.com>
    
    * cli: added server version
    
    Signed-off-by: Prajwal S N <prajwalnadig21@gmail.com>
    
    * cli(version): repaired error handling
    
    Signed-off-by: Prajwal S N <prajwalnadig21@gmail.com>
    
    * cli(version): better error handling
    
    Signed-off-by: Prajwal S N <prajwalnadig21@gmail.com>
    
    * cli(version): add review changes
    
    Signed-off-by: Prajwal S N <prajwalnadig21@gmail.com>
    
    Co-authored-by: Kent Rancourt <kent.rancourt@microsoft.com>

--- sdk/v2/system/api_client.go
+++ sdk/v2/system/api_client.go
@@ -64,11 +64,10 @@ func (a *apiClient) UnversionedPing(ctx context.Context) ([]byte, error) {
 			SuccessCode: http.StatusOK,
 		},
 	)
-	defer resp.Body.Close()
 	if err != nil {
 		return nil, errors.Wrap(err, "error submitting request")
 	}
-
+	defer resp.Body.Close()
 	respBodyBytes, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return nil, errors.Wrap(err, "error reading response body")
--- v2/cli/flags.go
+++ v2/cli/flags.go
@@ -7,6 +7,7 @@ const (
 	flagAnyPhase       = "any-phase"
 	flagBrowse         = "browse"
 	flagCanceled       = "canceled"
+	flagClient         = "client"
 	flagContainer      = "container"
 	flagContinue       = "continue"
 	flagDescription    = "description"
