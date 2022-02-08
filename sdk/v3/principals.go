6323ad12eeda787f2ef58c0d6c4e0e23731935ef
Kent Rancourt <kent.rancourt@microsoft.com>
2022-02-07 19:26:29 -0500

    fix role/project-role grant/revoke bug (#1805)
    Signed-off-by: Kent Rancourt <kent.rancourt@microsoft.com>

--- sdk/v3/principals.go
+++ sdk/v3/principals.go
@@ -1,15 +1,5 @@
 package sdk
 
-import (
-	"encoding/json"
-
-	"github.com/brigadecore/brigade/sdk/v3/meta"
-)
-
-// PrincipalReferenceKind represents the canonical PrincipalReference kind
-// string
-const PrincipalReferenceKind = "PrincipalReference"
-
 // PrincipalType is a type whose values can be used to disambiguate one type of
 // principal from another. For instance, when assigning a Role to a principal
 // via a RoleAssignment, a PrincipalType field is used to indicate whether the
@@ -25,22 +15,4 @@ type PrincipalReference struct {
 	// ID references a principal. The Type qualifies what type of principal that
 	// is-- for instance, a User or a ServiceAccount.
 	ID string `json:"id,omitempty"`
-}
-
-// MarshalJSON amends PrincipalReference instances with type metadata so that
-// clients do not need to be concerned with the tedium of doing so.
-func (p PrincipalReference) MarshalJSON() ([]byte, error) {
-	type Alias PrincipalReference
-	return json.Marshal(
-		struct {
-			meta.TypeMeta `json:",inline"`
-			Alias         `json:",inline"`
-		}{
-			TypeMeta: meta.TypeMeta{
-				APIVersion: meta.APIVersion,
-				Kind:       PrincipalReferenceKind,
-			},
-			Alias: (Alias)(p),
-		},
-	)
 }
