38c3bcebd8478a35726a746e17242408b3c3dc1b
Kent Rancourt <kent.rancourt@microsoft.com>
2022-02-17 11:30:28 -0500

    streamline internal amqp components
    Signed-off-by: Kent Rancourt <kent.rancourt@microsoft.com>

--- v2/internal/amqp/receiver.go
+++ v2/internal/amqp/receiver.go
@@ -18,15 +18,3 @@ type Receiver interface {
 	// Close closes the Sender and AMQP link.
 	Close(ctx context.Context) error
 }
-
-type receiver struct {
-	receiver *amqp.Receiver
-}
-
-func (r *receiver) Receive(ctx context.Context) (*amqp.Message, error) {
-	return r.receiver.Receive(ctx)
-}
-
-func (r *receiver) Close(ctx context.Context) error {
-	return r.receiver.Close(ctx)
-}
