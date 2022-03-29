6951041e228972331f8342796864f2a71403212b
Marius Kimmina <mar.kimmina@gmail.com>
2022-03-29 17:32:17 +0200

    adjust nightly-cleanup script to use table option
    Signed-off-by: Marius Kimmina <mar.kimmina@gmail.com>

--- scripts/nightly-cleanup.sh
+++ scripts/nightly-cleanup.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
 az login --service-principal --username $AZ_USERNAME --password $AZ_PASSWORD --tenant $AZ_TENANT
-repo_list=$(az acr repository list --name unstablebrigade)
+repo_list=$(az acr repository list --name unstablebrigade -o table | tail -n +3)
 
-# remove leading and trailing []
-repo_list=${repo_list::-1}
-clean_repo_list=${repo_list:1}
-
-for repo in ${clean_repo_list}
+for repo in ${repo_list}
 do
-    repo=$(echo $repo | tr -d '"')
     az acr repository delete --name unstablebrigade --repository ${repo} --yes
 done
